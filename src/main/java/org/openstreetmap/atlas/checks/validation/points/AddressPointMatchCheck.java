9c2eadc5a035a4792642fad0a08f2afb61a3eadc
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-03-21 15:42:40 -0700

    list all possible streets

--- src/main/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheck.java
@@ -1,7 +1,10 @@
 package org.openstreetmap.atlas.checks.validation.points;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-import com.google.common.collect.Iterables;
 import org.openstreetmap.atlas.checks.base.BaseCheck;
 import org.openstreetmap.atlas.checks.flag.CheckFlag;
 import org.openstreetmap.atlas.geography.Rectangle;
@@ -12,7 +15,7 @@ import org.openstreetmap.atlas.geography.atlas.items.Point;
 import org.openstreetmap.atlas.utilities.configuration.Configuration;
 import org.openstreetmap.atlas.utilities.scalars.Distance;
 
-import java.util.*;
+import com.google.common.collect.Iterables;
 
 public class AddressPointMatchCheck extends BaseCheck
 {
@@ -25,12 +28,14 @@ public class AddressPointMatchCheck extends BaseCheck
     private static final List<String> FALLBACK_INSTRUCTIONS = Arrays
             .asList(NO_STREET_NAME_INSTRUCTIONS, NO_SUGGESTED_NAMES_INSTRUCTIONS);
     private static final String ADDRESS_STREET_NUMBER_KEY = "addr:housenumber";
-    private static final String STREET_NAME_KEY = "addr:street";
+    private static final String POINT_STREET_NAME_KEY = "addr:street";
+    private static final String EDGE_STREET_NAME_KEY ="name";
     private static final double BOUNDS_SIZE_DEFAULT = 25.0;
 
     private final Distance boundsSize;
 
-    @Override protected List<String> getFallbackInstructions()
+    @Override
+    protected List<String> getFallbackInstructions()
     {
         return FALLBACK_INSTRUCTIONS;
     }
@@ -38,52 +43,79 @@ public class AddressPointMatchCheck extends BaseCheck
     public AddressPointMatchCheck(final Configuration configuration)
     {
         super(configuration);
-        this.boundsSize = Distance.meters((Double) configurationValue(configuration,
-                "bounds.size", BOUNDS_SIZE_DEFAULT));
+        this.boundsSize = Distance.meters(
+                (Double) configurationValue(configuration, "bounds.size", BOUNDS_SIZE_DEFAULT));
     }
 
-    @Override public boolean validCheckForObject(final AtlasObject object)
+    @Override
+    public boolean validCheckForObject(final AtlasObject object)
     {
+        // Object is an instance of Point
         return object instanceof Point
-                && !object.getTags().containsKey(ADDRESS_STREET_NUMBER_KEY);
+                // And has a street number specified
+                && object.getTag(ADDRESS_STREET_NUMBER_KEY).isPresent()
+                && (!object.getTag(POINT_STREET_NAME_KEY).isPresent()
+                    || !object.getTags().containsKey(POINT_STREET_NAME_KEY));
     }
 
     /**
      * This is the actual function that will check to see whether the object needs to be flagged.
      *
-     * @param object the atlas object supplied by the Atlas-Checks framework for evaluation
+     * @param object
+     *            the atlas object supplied by the Atlas-Checks framework for evaluation
      * @return an optional {@link CheckFlag} object that
      */
-    @Override protected Optional<CheckFlag> flag(final AtlasObject object)
+    @Override
+    protected Optional<CheckFlag> flag(final AtlasObject object)
     {
-        final Node node = (Node) object;
-        // Get a bounding box around the Node of interest
-        final Rectangle box = node.getLocation().boxAround(boundsSize);
+        final Point point = (Point) object;
+        // Get a bounding box around the Point of interest
+        final Rectangle box = point.getLocation().boxAround(boundsSize);
 
-        Iterable<Node> interiorNodes = node.getAtlas().nodesWithin(box);
-        Iterable<Edge> interiorEdges = node.getAtlas().edgesIntersecting(box);
-        List<String> streetNameList = new ArrayList<>();
+        // Get all Points inside the bounding box
+        Iterable<Point> interiorPoints = point.getAtlas().pointsWithin(box);
+        // Convert the Iterable into a Stream for filtering
+        Stream<Point> pointStream = StreamSupport.stream(interiorPoints.spliterator(), false);
+        // Remove Points that have null as their street name as they cannot be candidates for
+        // a street for the Point of interest
+        Set<Point> points = pointStream.filter(interiorPoint -> interiorPoint.getTag(POINT_STREET_NAME_KEY).isPresent())
+                .collect(Collectors.toSet());
 
-        // If there are no nodes or edges in the bounding box
-        if (Iterables.isEmpty(interiorEdges) && Iterables.isEmpty(interiorEdges)) {
-            // Flag node with instruction indicating that there are are no suggestions
-            return Optional.of(this.createFlag(node, this.getLocalizedInstruction(1,
-                    node.getOsmIdentifier()));
-        }
-        else if (!Iterables.isEmpty(interiorNodes))
+        // Get all Edges that intersect or are contained within the bounding box
+        Iterable<Edge> interiorEdges = point.getAtlas().edgesIntersecting(box);
+        // Convert the Iterable into a Stream for filtering
+        Stream<Edge> edgeStream = StreamSupport.stream(interiorEdges.spliterator(), false);
+        // Remove Edge that have null as their street name as they cannot be candidates for
+        // a street for the Point of interest
+        Set<Edge> edges = edgeStream.filter(interiorEdge -> interiorEdge.getTag(EDGE_STREET_NAME_KEY).isPresent())
+                .collect(Collectors.toSet());
+
+        Set<String> streetNameList = new HashSet<>();
+
+        // If there are no Points or Edges in the bounding box
+        if (points.isEmpty() && edges.isEmpty())
         {
-            interiorNodes.forEach(interiorNode ->
-                    streetNameList.add((interiorNode.getTags().get(STREET_NAME_KEY))));
+            // Flag Point with instruction indicating that there are are no suggestions
+            return Optional.of(this.createFlag(point,
+                    this.getLocalizedInstruction(1, point.getOsmIdentifier())));
+        }
+        // If there are Points in the bounding box
+        else if (!points.isEmpty())
+        {
+            // Add all interior Point street names to the list of candidate street names
+            points.forEach(interiorPoint -> streetNameList
+                    .add((interiorPoint.getTags().get(POINT_STREET_NAME_KEY))));
 
         }
-        else if (!Iterables.isEmpty(interiorEdges)){
-            interiorEdges.forEach(interiorEdge ->
-                    streetNameList.add((interiorEdge.getTags().get(STREET_NAME_KEY))));
+        // If there are Edges intersecting or contained by the bounding box
+        else
+        {
+            // Add all interior Edge street names to the list of candidate street names
+            edges.forEach(interiorEdge -> streetNameList
+                    .add((interiorEdge.getTags().get(EDGE_STREET_NAME_KEY))));
         }
 
-
-            return Optional.of(this.createFlag(node, this.getLocalizedInstruction(0,
-                    node.getOsmIdentifier(), streetNameList)));
-        }
+        return Optional.of(this.createFlag(point,
+                this.getLocalizedInstruction(0, point.getOsmIdentifier(), streetNameList)));
     }
 }
--- src/test/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheckTest.java
+++ src/test/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheckTest.java
@@ -1,5 +1,19 @@
 package org.openstreetmap.atlas.checks.validation.points;
 
+import org.junit.Rule;
+import org.openstreetmap.atlas.checks.validation.verifier.ConsumerBasedExpectedCheckVerifier;
+
+/**
+ * Tests for {@link AddressPointMatchCheck}
+ *
+ * @author savannahostrowski
+ */
 public class AddressPointMatchCheckTest
 {
+    @Rule
+    public AddressPointMatchCheckTestRule setup = new AddressPointMatchCheckTestRule();
+
+    @Rule
+    public ConsumerBasedExpectedCheckVerifier verifier = new ConsumerBasedExpectedCheckVerifier();
+
 }
