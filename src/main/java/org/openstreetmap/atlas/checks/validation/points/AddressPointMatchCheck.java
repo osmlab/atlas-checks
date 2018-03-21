f6f4b78af81455b359b6c3b9f37a1c24f6546352
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-03-21 14:22:08 -0700

    rethinking check; only address missing street name on points

--- src/main/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheck.java
@@ -1,32 +1,31 @@
 package org.openstreetmap.atlas.checks.validation.points;
 
+
 import com.google.common.collect.Iterables;
 import org.openstreetmap.atlas.checks.base.BaseCheck;
 import org.openstreetmap.atlas.checks.flag.CheckFlag;
-import org.openstreetmap.atlas.geography.PolyLine;
 import org.openstreetmap.atlas.geography.Rectangle;
-import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
 import org.openstreetmap.atlas.geography.atlas.items.Edge;
 import org.openstreetmap.atlas.geography.atlas.items.Node;
+import org.openstreetmap.atlas.geography.atlas.items.Point;
 import org.openstreetmap.atlas.utilities.configuration.Configuration;
 import org.openstreetmap.atlas.utilities.scalars.Distance;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class AddressPointMatchCheck extends BaseCheck
 {
     private static final long serialVersionUID = 1L;
-    public static final String WRONG_STREET_NAME_INSTRUCTIONS = "This node, {0,number,#}, has the "
-            + "incorrect street name tagged in the address. The street name should be {1}.";
-    public static final String NO_STREET_NAME_INSTRUCTIONS = "This node, {0,number,#}, has no "
-            + "street name tagged in the address. The street name should be {1}.";
+    public static final String NO_STREET_NAME_INSTRUCTIONS = "This node, {0,number,#}, has "
+            + "no street name specified in the address. The street name should likely "
+            + "be one of {1}.";
+    public static final String NO_SUGGESTED_NAMES_INSTRUCTIONS = "This node, {0,number,#}, has "
+            + "no street name specified in the address. No suggestions names were found.";
     private static final List<String> FALLBACK_INSTRUCTIONS = Arrays
-            .asList(WRONG_STREET_NAME_INSTRUCTIONS, NO_STREET_NAME_INSTRUCTIONS);
-    private static final String ADDRESS_STREET_KEY = "addr:street";
+            .asList(NO_STREET_NAME_INSTRUCTIONS, NO_SUGGESTED_NAMES_INSTRUCTIONS);
+    private static final String ADDRESS_STREET_NUMBER_KEY = "addr:housenumber";
+    private static final String STREET_NAME_KEY = "addr:street";
     private static final double BOUNDS_SIZE_DEFAULT = 25.0;
 
     private final Distance boundsSize;
@@ -39,13 +38,14 @@ public class AddressPointMatchCheck extends BaseCheck
     public AddressPointMatchCheck(final Configuration configuration)
     {
         super(configuration);
-        this.boundsSize = Distance.meters((Double) configurationValue(configuration, "bounds.size",
-                BOUNDS_SIZE_DEFAULT));
+        this.boundsSize = Distance.meters((Double) configurationValue(configuration,
+                "bounds.size", BOUNDS_SIZE_DEFAULT));
     }
 
     @Override public boolean validCheckForObject(final AtlasObject object)
     {
-        return object instanceof Node;
+        return object instanceof Point
+                && !object.getTags().containsKey(ADDRESS_STREET_NUMBER_KEY);
     }
 
     /**
@@ -59,61 +59,31 @@ public class AddressPointMatchCheck extends BaseCheck
         final Node node = (Node) object;
         // Get a bounding box around the Node of interest
         final Rectangle box = node.getLocation().boxAround(boundsSize);
-        double closestNodeDistance = Integer.MAX_VALUE;
-        Node closestNode = null;
 
-        // Try and find the closest Node to the Node of interest
-        getClosestNode(node, box, closestNode, closestNodeDistance);
+        Iterable<Node> interiorNodes = node.getAtlas().nodesWithin(box);
+        Iterable<Edge> interiorEdges = node.getAtlas().edgesIntersecting(box);
+        List<String> streetNameList = new ArrayList<>();
 
-
-
-
-        // If we have found the closest Node to the Node of interest (so it exists)
-        if (closestNode != null)
+        // If there are no nodes or edges in the bounding box
+        if (Iterables.isEmpty(interiorEdges) && Iterables.isEmpty(interiorEdges)) {
+            // Flag node with instruction indicating that there are are no suggestions
+            return Optional.of(this.createFlag(node, this.getLocalizedInstruction(1,
+                    node.getOsmIdentifier()));
+        }
+        else if (!Iterables.isEmpty(interiorNodes))
         {
-            // Flag the node with the proper street name in the MapRoulette instructions
+            interiorNodes.forEach(interiorNode ->
+                    streetNameList.add((interiorNode.getTags().get(STREET_NAME_KEY))));
+
+        }
+        else if (!Iterables.isEmpty(interiorEdges)){
+            interiorEdges.forEach(interiorEdge ->
+                    streetNameList.add((interiorEdge.getTags().get(STREET_NAME_KEY))));
+        }
+
+
             return Optional.of(this.createFlag(node, this.getLocalizedInstruction(0,
-                    node.getOsmIdentifier(), closestNode.getTag(ADDRESS_STREET_KEY))));
-        }
-        // If we haven't found a closest Node, then we need to check for streets that intersect
-        // With our bounding box
-
-
-        // If we haven't found an any Edges (or any Nodes), then flag the Node but state that we
-        // are not certain of the proper street name for that Node. The proper street name should
-        // be decided at the discretion of the editor
-        return Optional.of(this.createFlag(node, this.getLocalizedInstruction(1,
-                node.getOsmIdentifier())));
-    }
-
-    private Node getClosestNode(Node node, Rectangle boundingBox, Node closestNodeObj, double closestNodeDistanceVal) {
-        final Iterable<Node> interiorNodes = node.getAtlas().nodesWithin(boundingBox);
-
-        // If there are nodes within the bounding box (aside from the Node of interest
-        if (!Iterables.isEmpty(interiorNodes))
-        {
-            // For each node in the bounding box
-            for (Node interiorNode : interiorNodes)
-            {
-                // If node in the bounding box has a street name
-                if (interiorNode.getTags().containsKey(ADDRESS_STREET_KEY))
-                {
-                    PolyLine nodesAsPolyline = new PolyLine(node.getLocation(),
-                            interiorNode.getLocation());
-                    double betweenNodeDistance = nodesAsPolyline.length().asMeters();
-
-                    // If a interior Node has not yet been compared to the Node of interest
-                    // Or if the interior Node to the Node of interest distance is shorter
-                    // Than the closestNodeDistance
-                    if (closestNodeObj == null || closestNodeDistanceVal > betweenNodeDistance)
-                    {
-                        closestNodeObj = interiorNode;
-                        closestNodeDistanceVal = betweenNodeDistance;
-
-                    }
-                }
-            }
+                    node.getOsmIdentifier(), streetNameList)));
         }
     }
-
 }
