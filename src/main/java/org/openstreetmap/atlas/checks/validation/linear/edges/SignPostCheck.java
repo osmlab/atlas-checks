0a8c30a5428ae5586abbdd49077bf26ea44683b9
Bentley Breithaupt <bentleysb@gmail.com>
2018-06-11 09:53:45 -0700

    Removed old heading test

--- src/main/java/org/openstreetmap/atlas/checks/validation/linear/edges/SignPostCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/linear/edges/SignPostCheck.java
@@ -10,7 +10,6 @@ import org.apache.directory.api.util.Strings;
 import org.openstreetmap.atlas.checks.atlas.predicates.TypePredicates;
 import org.openstreetmap.atlas.checks.base.BaseCheck;
 import org.openstreetmap.atlas.checks.flag.CheckFlag;
-import org.openstreetmap.atlas.geography.Heading;
 import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
 import org.openstreetmap.atlas.geography.atlas.items.Edge;
 import org.openstreetmap.atlas.geography.atlas.items.Node;
@@ -206,9 +205,7 @@ public class SignPostCheck extends BaseCheck<String>
 
         // Go back and collect edges
         final List<Edge> endEdges = new ArrayList<>();
-        // Check that we are not over the max and the edge haas a heading
-        final Optional<Heading> initialHeading = finalEdge.asPolyLine().initialHeading();
-        if (count < maxEdgeCount && initialHeading.isPresent())
+        if (count < maxEdgeCount)
         {
             // Collect in edges and make sure the list is not empty
             final Set<Edge> inEdges = finalEdge.inEdges();
