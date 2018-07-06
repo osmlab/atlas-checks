17785c646e3a91cb3afced9dc1a8ca7f3e267fd5
Bentley Breithaupt <bentleysb@gmail.com>
2018-07-06 15:58:05 -0700

    refactored hasMinimumOverlapProportion into a utility

--- src/main/java/org/openstreetmap/atlas/checks/validation/areas/OverlappingAOIPolygonCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/areas/OverlappingAOIPolygonCheck.java
@@ -8,17 +8,14 @@ import java.util.Set;
 
 import org.openstreetmap.atlas.checks.base.BaseCheck;
 import org.openstreetmap.atlas.checks.flag.CheckFlag;
-import org.openstreetmap.atlas.geography.PolyLine;
+import org.openstreetmap.atlas.checks.validation.utility.IntersectionUtilities;
 import org.openstreetmap.atlas.geography.Polygon;
 import org.openstreetmap.atlas.geography.Rectangle;
 import org.openstreetmap.atlas.geography.atlas.items.Area;
 import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
-import org.openstreetmap.atlas.geography.clipping.Clip;
 import org.openstreetmap.atlas.tags.filters.TaggableFilter;
 import org.openstreetmap.atlas.utilities.collections.Iterables;
 import org.openstreetmap.atlas.utilities.configuration.Configuration;
-
-import com.vividsolutions.jts.geom.TopologyException;
 
 /**
  * This check flags {@link Area}s that overlap and represent the same Area of Interest (AOI) AOIs
@@ -117,8 +114,8 @@ public class OverlappingAOIPolygonCheck extends BaseCheck
         // the object
         for (final Area area : overlappingAreas)
         {
-            if (this.hasMinimumOverlapProportion(aoiPolygon, area.asPolygon())
-                    && aoiFiltersTest(object, area))
+            if (IntersectionUtilities.findIntersectionType(aoiPolygon,
+                    area.asPolygon()) >= this.minimumIntersect && aoiFiltersTest(object, area))
             {
                 flag.addObject(area);
                 flag.addInstruction(this.getLocalizedInstruction(0, object.getOsmIdentifier(),
@@ -141,55 +138,6 @@ public class OverlappingAOIPolygonCheck extends BaseCheck
     protected List<String> getFallbackInstructions()
     {
         return FALLBACK_INSTRUCTIONS;
-    }
-
-    /**
-     * Uses code from findIntersectionType function in BuildingRoadIntersection
-     * 
-     * @param polygon
-     *            {@link Polygon} to check for intersection
-     * @param otherPolygon
-     *            Another {@link Polygon} to check against for intersection
-     * @return true if the polygon to polygon overlap surface area is greater than minimum
-     */
-    private boolean hasMinimumOverlapProportion(final Polygon polygon, final Polygon otherPolygon)
-    {
-        Clip clip = null;
-        try
-        {
-            clip = polygon.clip(otherPolygon, Clip.ClipType.AND);
-        }
-        catch (final TopologyException e)
-        {
-            System.out
-                    .println(String.format("Error clipping [%s] and [%s].", polygon, otherPolygon));
-        }
-        // Skip if nothing is returned
-        if (clip == null)
-        {
-            return false;
-        }
-        // Sum intersection area
-        long intersectionArea = 0;
-        for (final PolyLine polyline : clip.getClip())
-        {
-            if (polyline != null && polyline instanceof Polygon)
-            {
-                final Polygon clippedPolygon = (Polygon) polyline;
-                intersectionArea += clippedPolygon.surface().asDm7Squared();
-            }
-        }
-        // Avoid division by zero
-        if (intersectionArea == 0)
-        {
-            return false;
-        }
-        // Pick the smaller building's area as baseline
-        final long baselineArea = Math.min(polygon.surface().asDm7Squared(),
-                otherPolygon.surface().asDm7Squared());
-        final double proportion = (double) intersectionArea / baselineArea;
-
-        return proportion >= this.minimumIntersect;
     }
 
     /**
