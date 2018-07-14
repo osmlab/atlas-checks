37c69154b891324c60b8433193bb271b55b86de0
matthieun <matthieu.nahoum@gmail.com>
2018-07-13 17:33:25 -0700

    Update gradle, add sonar and jacoco

--- .travis/build.sh
+++ .travis/build.sh
@@ -20,3 +20,14 @@ else
 	echo "Temporarily skip integration tests in all builds. Too heavy for Travis"
 	./gradlew clean build -x integrationTest
 fi
+
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ];
+then
+	echo "Running sonarqube in a CRON build"
+	./gradlew sonarqube \
+		-Dsonar.organization=osmlab \
+		-Dsonar.host.url=https://sonarcloud.io \
+		-Dsonar.login=$SONAR_TOKEN \
+		-Dsonar.junit.reportPaths=build/test-results/test \
+		-Dsonar.jacoco.reportPaths=build/jacoco/test.exec
+fi
