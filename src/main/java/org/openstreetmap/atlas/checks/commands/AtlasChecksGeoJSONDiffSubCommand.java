5928716889b55d61e47204e3cb0bb690f5567def
Bentley Breithaupt <bentleysb@gmail.com>
2018-08-31 10:19:03 -0700

    docs and comments

--- src/main/java/org/openstreetmap/atlas/checks/commands/AtlasChecksGeoJSONDiffSubCommand.java
+++ src/main/java/org/openstreetmap/atlas/checks/commands/AtlasChecksGeoJSONDiffSubCommand.java
@@ -29,6 +29,10 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.stream.JsonReader;
 
 /**
+ * Takes 2 sets of atlas-checks geoJSON flag files and reports the number of additions,
+ * subtractions, and changed flags from source to target. Optionally, the reported items can be
+ * written to new geoJSON files.
+ *
  * @author bbreithaupt
  */
 public class AtlasChecksGeoJSONDiffSubCommand implements FlexibleSubCommand
@@ -116,6 +120,15 @@ public class AtlasChecksGeoJSONDiffSubCommand implements FlexibleSubCommand
                 "-output=path/to/output/folder : optional directory to write output files to\n");
     }
 
+    /**
+     * Takes an {@link InputStream} of a json file, reads it, and returns it as a
+     * {@link JsonObject}.
+     *
+     * @param input
+     *            {@link InputStream} to read
+     * @return a {@link JsonObject} representation of the json file
+     * @throws IOException
+     */
     private JsonObject jsonReader(final InputStream input) throws IOException
     {
         final JsonReader reader = new JsonReader(new InputStreamReader(input));
@@ -123,6 +136,13 @@ public class AtlasChecksGeoJSONDiffSubCommand implements FlexibleSubCommand
         return gson.fromJson(reader, JsonObject.class);
     }
 
+    /**
+     * Parses an atlas-checks geoJSON flag file and maps each feature to its 'id' parameter.
+     *
+     * @param stream
+     *            {@link InputStream} of an atlas-checks geoJSON flag file
+     * @return {@link HashMap} of {@link JsonObject}s representing geoJSON features, mapped by id
+     */
     private HashMap<String, JsonObject> mapFeatures(final InputStream stream)
     {
         final HashMap featureMap = new HashMap<String, JsonObject>();
@@ -142,6 +162,20 @@ public class AtlasChecksGeoJSONDiffSubCommand implements FlexibleSubCommand
         return featureMap;
     }
 
+    /**
+     * Takes two {@link HashMap}s containing atlas-checks geoJSON features mapped by id. Finds
+     * missing elements in the target based on keys (geoJSON feature 'id' parameter). Optionally
+     * computes changes in the Atlas object ids found in each feature.
+     *
+     * @param source
+     *            {@link HashMap} of the features to compare from
+     * @param target
+     *            {@link HashMap} of the features to compare to
+     * @param onlyMissing
+     *            {@code boolean} value to not calculate changes in Atlas object ids
+     * @return an {@link ArrayList} of 2 {@link HashSet}s of {@link JsonObject}s, the first being
+     *         the missing features and the second being the changed features
+     */
     private ArrayList<HashSet<JsonObject>> getMissingAndChanged(
             final HashMap<String, JsonObject> source, final HashMap<String, JsonObject> target,
             final boolean onlyMissing)
@@ -150,10 +184,12 @@ public class AtlasChecksGeoJSONDiffSubCommand implements FlexibleSubCommand
         final HashSet<JsonObject> changed = new HashSet<>();
         source.forEach((identifier, feature) ->
         {
+            // Get missing
             if (!target.containsKey(identifier))
             {
                 missing.add(feature);
             }
+            // If not missing, check for Atlas id changes
             else if (!onlyMissing && !identicalFeatureIds(
                     feature.get("properties").getAsJsonObject().get("feature_properties")
                             .getAsJsonArray(),
@@ -169,6 +205,15 @@ public class AtlasChecksGeoJSONDiffSubCommand implements FlexibleSubCommand
         return missingAndChanged;
     }
 
+    /**
+     * Helper function for {@code getMissingAndChanged} to parse and check for changes in Atlas ids.
+     *
+     * @param sourceArray
+     *            {@code feature_properties} {@link JsonArray} to check from
+     * @param targetArray
+     *            {@code feature_properties} {@link JsonArray} to check to
+     * @return true if all Atlas ids in {@code source} are present in {@code target}, and visa versa
+     */
     private boolean identicalFeatureIds(final JsonArray sourceArray, final JsonArray targetArray)
     {
         final ArrayList<String> sourceIds = new ArrayList<>();
@@ -180,6 +225,14 @@ public class AtlasChecksGeoJSONDiffSubCommand implements FlexibleSubCommand
         return sourceIds.containsAll(targetIds) && targetIds.containsAll(sourceIds);
     }
 
+    /**
+     * Writes a Set of geoJSON features to a geoJSON file.
+     *
+     * @param features
+     *            {@link Set} of {@link JsonObject}s representing geoJSON features
+     * @param output
+     *            {@link File} to output to
+     */
     private void writeSetToGeoJSON(final Set<JsonObject> features, final File output)
     {
         final JsonArray featureArray = new JsonArray();
