88015d61d529101f71b675ced3617b682d4fac17
Bentley Breithaupt <bentleysb@gmail.com>
2018-09-12 13:46:49 -0700

    more temp dirs

--- src/test/java/org/openstreetmap/atlas/checks/commands/AtlasChecksLogDiffSubCommandTest.java
+++ src/test/java/org/openstreetmap/atlas/checks/commands/AtlasChecksLogDiffSubCommandTest.java
@@ -1,8 +1,13 @@
 package org.openstreetmap.atlas.checks.commands;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openstreetmap.atlas.streaming.resource.File;
 
@@ -13,14 +18,40 @@ import org.openstreetmap.atlas.streaming.resource.File;
  */
 public class AtlasChecksLogDiffSubCommandTest
 {
-    private static final String GEO_11 = AtlasChecksGeoJSONDiffSubCommandTest.class
-            .getResource("source/geo_11.log").getPath();
-    private static final String GEO_21 = AtlasChecksGeoJSONDiffSubCommandTest.class
-            .getResource("target/geo_21.log").getPath();
-    private static final String SOURCE_DIRECTORY = AtlasChecksGeoJSONDiffSubCommandTest.class
-            .getResource("source").getPath();
-    private static final String TARGET_DIRECTORY = AtlasChecksGeoJSONDiffSubCommandTest.class
-            .getResource("target").getPath();
+    private static final File SOURCE_DIRECTORY = File.temporaryFolder();
+    private static final File TARGET_DIRECTORY = File.temporaryFolder();
+    private static final String GEO_11 = AtlasChecksLogDiffSubCommandTest.class
+            .getResource("geo_11.log").getPath();
+    private static final String GEO_21 = AtlasChecksLogDiffSubCommandTest.class
+            .getResource("geo_21.log").getPath();
+
+    @BeforeClass
+    public static void copyLogFiles() throws IOException
+    {
+        Files.copy(
+                Paths.get(
+                        AtlasChecksLogDiffSubCommandTest.class.getResource("geo_11.log").getPath()),
+                Paths.get(SOURCE_DIRECTORY.getPath() + "/geo_11.log"));
+        Files.copy(
+                Paths.get(
+                        AtlasChecksLogDiffSubCommandTest.class.getResource("geo_12.log").getPath()),
+                Paths.get(SOURCE_DIRECTORY.getPath() + "/geo_12.log"));
+        Files.copy(
+                Paths.get(
+                        AtlasChecksLogDiffSubCommandTest.class.getResource("geo_21.log").getPath()),
+                Paths.get(TARGET_DIRECTORY.getPath() + "/geo_21.log"));
+        Files.copy(
+                Paths.get(
+                        AtlasChecksLogDiffSubCommandTest.class.getResource("geo_22.log").getPath()),
+                Paths.get(TARGET_DIRECTORY.getPath() + "/geo_22.log"));
+    }
+
+    @AfterClass
+    public static void deleteLogFiles()
+    {
+        SOURCE_DIRECTORY.deleteRecursively();
+        TARGET_DIRECTORY.deleteRecursively();
+    }
 
     @Test
     public void fileCreationFromFileTest()
