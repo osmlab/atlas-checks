030a5b5f48f29c02d05d7815057b9ef4220a5a9e
Bentley Breithaupt <bentleysb@gmail.com>
2018-10-02 11:59:18 -0700

    Flag File Diff Commands (#72)
    * add AtlasChecksCommand and AtlasChecksGeoJSONDiffSubCommand
    
    * multi file supprt; file output; linter cleanup
    
    * docs and comments
    
    * geo diff unit test
    
    * add log diff command
    
    * refactored to use abstract class
    
    * unit test files
    
    * refactor getMissingAndChanged
    
    * change AbstractJsonFlagDiffCommand name
    
    * refactor to use new abstract class name
    
    * refactor json strings
    
    * use dir instead of csv's
    
    * update test resources
    
    * update argument descriptions
    
    * remove old resources
    
    * this; abstract; docs
    
    * more temp dirs
    
    * rename command param
    
    * handle location props

---
+++ src/main/java/org/openstreetmap/atlas/checks/commands/AtlasChecksLogDiffSubCommand.java
@@ -0,0 +1,134 @@
+package org.openstreetmap.atlas.checks.commands;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.openstreetmap.atlas.streaming.resource.File;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Takes 2 sets of atlas-checks log files and reports the number of additions, subtractions, and
+ * changed flags from source to target. Optionally, the reported items can be written to new log
+ * files. Additions and subtractions are based on flag ids. Changes are calculated by differences in
+ * the Atlas ids of objects in a flag.
+ *
+ * @author bbreithaupt
+ */
+public class AtlasChecksLogDiffSubCommand extends JSONFlagDiffSubCommand
+{
+    public AtlasChecksLogDiffSubCommand()
+    {
+        super("log-diff",
+                "Takes 2 sets of atlas-checks log flag files and reports the number of additions, subtractions, and changed flags from source to target.",
+                "log");
+    }
+
+    @Override
+    protected void mapFeatures(final File file, final HashMap map)
+    {
+        try
+        {
+            final BufferedReader reader = new BufferedReader(new FileReader(file.getPath()));
+            String line;
+            // Read each line (flag) from the log file
+            while ((line = reader.readLine()) != null)
+            {
+                // Parse the json
+                final JsonObject source = getGson().fromJson(line, JsonObject.class);
+                // Get the check name
+                final String checkName = source.get(PROPERTIES).getAsJsonObject().get(GENERATOR)
+                        .getAsString();
+                // Add the check name as a key
+                if (!map.containsKey(checkName))
+                {
+                    map.put(checkName, new HashMap<>());
+                }
+                // Add the geoJSON as a value
+                ((HashMap<String, HashMap>) map).get(checkName).put(
+                        source.get(PROPERTIES).getAsJsonObject().get(ID).getAsString(), source);
+            }
+        }
+        catch (final IOException exc)
+        {
+            exc.printStackTrace();
+        }
+    }
+
+    @Override
+    protected JSONFlagDiff getDiff(final HashMap source, final HashMap target,
+            final DiffReturn returnType)
+    {
+        final JSONFlagDiff diff = new JSONFlagDiff();
+        source.forEach((check, flag) ->
+        {
+            ((HashMap<String, JsonObject>) flag).forEach((identifier, featureCollection) ->
+            {
+                // Get missing
+                if (!target.containsKey(check)
+                        || !((HashMap<String, HashMap>) target).get(check).containsKey(identifier))
+                {
+                    diff.addMissing(featureCollection);
+                }
+                // If not missing, check for Atlas id changes
+                else if (returnType.equals(DiffReturn.CHANGED) && !this.identicalFeatureIds(
+                        featureCollection.get(FEATURES).getAsJsonArray(),
+                        ((HashMap<String, HashMap<String, JsonObject>>) target).get(check)
+                                .get(identifier).get(FEATURES).getAsJsonArray()))
+                {
+                    diff.addChanged(featureCollection);
+                }
+            });
+        });
+        return diff;
+    }
+
+    @Override
+    protected boolean identicalFeatureIds(final JsonArray sourceArray, final JsonArray targetArray)
+    {
+        final ArrayList<String> sourceIds = new ArrayList<>();
+        final ArrayList<String> targetIds = new ArrayList<>();
+        // The array must be the same size to match
+        if (sourceArray.size() != targetArray.size())
+        {
+            return false;
+        }
+        // Gather all the source ids
+        sourceArray.forEach(object ->
+        {
+            // Handle Locations that were added and don't have an id
+            if (object.getAsJsonObject().get(PROPERTIES).getAsJsonObject().has(ITEM_ID))
+            {
+                sourceIds.add(object.getAsJsonObject().get(PROPERTIES).getAsJsonObject()
+                        .get(ITEM_ID).getAsString());
+            }
+        });
+        // Gather all the target ids
+        targetArray.forEach(object ->
+        {
+            // Handle Locations that were added and don't have an id
+            if (object.getAsJsonObject().get(PROPERTIES).getAsJsonObject().has(ITEM_ID))
+            {
+                targetIds.add(object.getAsJsonObject().get(PROPERTIES).getAsJsonObject()
+                        .get(ITEM_ID).getAsString());
+            }
+        });
+        // Compare the two id lists
+        return sourceIds.containsAll(targetIds) && targetIds.containsAll(sourceIds);
+    }
+
+    @Override
+    protected int getSourceSize()
+    {
+        int sourceSize = 0;
+        for (final String check : ((HashMap<String, HashMap>) getSource()).keySet())
+        {
+            sourceSize += ((HashMap<String, HashMap>) getSource()).get(check).size();
+        }
+        return sourceSize;
+    }
+}
---
+++ src/test/java/org/openstreetmap/atlas/checks/commands/AtlasChecksGeoJSONDiffSubCommandTest.java
@@ -0,0 +1,94 @@
+package org.openstreetmap.atlas.checks.commands;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openstreetmap.atlas.streaming.resource.File;
+
+/**
+ * Unit tests for {@link AtlasChecksGeoJSONDiffSubCommand}.
+ *
+ * @author bbreithaupt
+ */
+public class AtlasChecksGeoJSONDiffSubCommandTest
+{
+    private static final File SOURCE_DIRECTORY = File.temporaryFolder();
+    private static final File TARGET_DIRECTORY = File.temporaryFolder();
+    private static final String GEO_11 = AtlasChecksGeoJSONDiffSubCommandTest.class
+            .getResource("geo_11.geojson").getPath();
+    private static final String GEO_21 = AtlasChecksGeoJSONDiffSubCommandTest.class
+            .getResource("geo_21.geojson").getPath();
+
+    @BeforeClass
+    public static void copyLogFiles() throws IOException
+    {
+        Files.copy(Paths.get(
+                AtlasChecksGeoJSONDiffSubCommandTest.class.getResource("geo_11.geojson").getPath()),
+                Paths.get(SOURCE_DIRECTORY.getPath() + "/geo_11.geojson"));
+        Files.copy(Paths.get(
+                AtlasChecksGeoJSONDiffSubCommandTest.class.getResource("geo_12.geojson").getPath()),
+                Paths.get(SOURCE_DIRECTORY.getPath() + "/geo_12.geojson"));
+        Files.copy(Paths.get(
+                AtlasChecksGeoJSONDiffSubCommandTest.class.getResource("geo_21.geojson").getPath()),
+                Paths.get(TARGET_DIRECTORY.getPath() + "/geo_21.geojson"));
+        Files.copy(Paths.get(
+                AtlasChecksGeoJSONDiffSubCommandTest.class.getResource("geo_22.geojson").getPath()),
+                Paths.get(TARGET_DIRECTORY.getPath() + "/geo_22.geojson"));
+    }
+
+    @AfterClass
+    public static void deleteLogFiles()
+    {
+        SOURCE_DIRECTORY.deleteRecursively();
+        TARGET_DIRECTORY.deleteRecursively();
+    }
+
+    @Test
+    public void fileCreationFromFileTest()
+    {
+        final File temp = File.temporaryFolder();
+
+        // Run AtlasJoinerSubCommand
+        final String[] args = { "geojson-diff", String.format("-source=%s", GEO_11),
+                String.format("-target=%s", GEO_21), String.format("-output=%s", temp.getPath()) };
+        new AtlasChecksCommand(args).runWithoutQuitting(args);
+
+        final List<File> outputFiles = temp.listFilesRecursively();
+        Assert.assertTrue(outputFiles.stream()
+                .anyMatch(file -> file.getName().matches("additions-\\d+-1.geojson")));
+        Assert.assertTrue(outputFiles.stream()
+                .anyMatch(file -> file.getName().matches("changes-\\d+-1.geojson")));
+        Assert.assertTrue(outputFiles.stream()
+                .anyMatch(file -> file.getName().matches("subtractions-\\d+-1.geojson")));
+
+        temp.deleteRecursively();
+    }
+
+    @Test
+    public void fileCreationFromDirectoryTest()
+    {
+        final File temp = File.temporaryFolder();
+
+        // Run AtlasJoinerSubCommand
+        final String[] args = { "geojson-diff", String.format("-source=%s", SOURCE_DIRECTORY),
+                String.format("-target=%s", TARGET_DIRECTORY),
+                String.format("-output=%s", temp.getPath()) };
+        new AtlasChecksCommand(args).runWithoutQuitting(args);
+
+        final List<File> outputFiles = temp.listFilesRecursively();
+        Assert.assertTrue(outputFiles.stream()
+                .anyMatch(file -> file.getName().matches("additions-\\d+-1.geojson")));
+        Assert.assertTrue(outputFiles.stream()
+                .anyMatch(file -> file.getName().matches("changes-\\d+-2.geojson")));
+        Assert.assertTrue(outputFiles.stream()
+                .anyMatch(file -> file.getName().matches("subtractions-\\d+-3.geojson")));
+
+        temp.deleteRecursively();
+    }
+}
