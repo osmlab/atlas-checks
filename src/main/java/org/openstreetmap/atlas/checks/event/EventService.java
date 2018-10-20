e08c2f6a6bc5b914229877b096fa858f8685114a
Jack Klamer <jfklamer@gmail.com>
2018-10-20 15:04:15 -0700

    Fix bug (#82)

--- src/main/java/org/openstreetmap/atlas/checks/event/EventService.java
+++ src/main/java/org/openstreetmap/atlas/checks/event/EventService.java
@@ -62,7 +62,7 @@ public final class EventService
         }
 
         this.eventBus.post(new ShutdownEvent());
-        this.processors.forEach(this::unregister);
+        new HashSet<>(this.processors).forEach(this::unregister);
     }
 
     /**
