543e749d7b79e9b4e394dd1634f9d457e39da66b
matthieun <matthieun@users.noreply.github.com>
2018-12-11 11:16:51 -0800

    HACK to upload artifacts to Sonatype (#105)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,13 +2,9 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-	echo "Sign Archives"
-	./gradlew signArchives
-	echo "Upload Archives"
-	./gradlew uploadArchives
 	if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
 	then
-		echo "Promote repository"
-		./gradlew closeAndReleaseRepository
+		echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
+		./gradlew uploadArchives publishToNexus closeAndReleaseRepository
 	fi
 fi
