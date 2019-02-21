05f81da43ed12118e48b59e7910f823c3e5b2be8
Yun Hu <yun_hu@apple.com>
2019-02-21 13:26:17 -0800

    merge changes with upstream

--- src/main/java/org/openstreetmap/atlas/checks/maproulette/data/Challenge.java
+++ src/main/java/org/openstreetmap/atlas/checks/maproulette/data/Challenge.java
@@ -60,7 +60,7 @@ public class Challenge implements Serializable
     public Challenge(final String name, final String description, final String blurb,
             final String instruction, final ChallengeDifficulty difficulty, final String tags)
     {
-        this(name, description, blurb, instruction, difficulty, ChallengePriority.NONE, null, null,
+        this(name, description, blurb, instruction, difficulty, ChallengePriority.LOW, null, null,
                 null, tags);
     }
 
@@ -68,7 +68,7 @@ public class Challenge implements Serializable
             final String instruction, final String checkinComment,
             final ChallengeDifficulty difficulty, final String tags)
     {
-        this(name, description, blurb, instruction, difficulty, ChallengePriority.NONE, null, null,
+        this(name, description, blurb, instruction, difficulty, ChallengePriority.LOW, null, null,
                 null, tags);
         this.checkinComment = checkinComment;
     }
