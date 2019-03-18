4322e00eb599b06583374d38556e8cd050302368
Micah Nacht <mjnacht@gmail.com>
2019-03-18 15:21:24 -0700

    strip out parseWithProject

--- src/main/java/org/openstreetmap/atlas/checks/maproulette/MapRouletteConfiguration.java
+++ src/main/java/org/openstreetmap/atlas/checks/maproulette/MapRouletteConfiguration.java
@@ -44,81 +44,25 @@ public class MapRouletteConfiguration implements Serializable
             final String[] components = configuration.split(DELIMITER);
             if (components.length == NUMBER_OF_COMPONENTS)
             {
-                final ProjectConfiguration projectConfiguration = new ProjectConfiguration(
+                ProjectConfiguration projectConfiguration = new ProjectConfiguration(
                         components[PROJECT_NAME_INDEX]);
-                return MapRouletteConfiguration.parseWithProject(components, projectConfiguration);
+                if (!projectConfiguration.getName().equals(components[PROJECT_NAME_INDEX]))
+                {
+                    logger.warn(
+                            "Project name from string ({}) does not equal name from configuration object ({}). Using name from string.",
+                            components[PROJECT_NAME_INDEX], projectConfiguration);
+                    projectConfiguration = new ProjectConfiguration(components[PROJECT_NAME_INDEX],
+                            projectConfiguration.getDescription(),
+                            projectConfiguration.getDisplayName(), PROJECT_DEFAULT_ENABLED);
+                }
+                return new MapRouletteConfiguration(components[SERVER_INDEX],
+                        Integer.parseInt(components[PORT_INDEX]), projectConfiguration,
+                        components[API_KEY_INDEX]);
             }
         }
         logger.debug(
                 String.format("Map Roulette configuration not set, invalid string passed in. [%s]",
                         configuration));
-        return null;
-    }
-
-    /**
-     * Parses a map roulette configuration object from a string that follows the structure
-     * [SERVER]:[PORT]:[PROJECT_NAME]:[API_KEY] and a project-specific configuration object. Note
-     * that the project configuration object also contains a project name -- if the two differ, a
-     * warning will be logged and the project name from the string will be used.
-     * 
-     * @param configuration
-     *            The configuration string to parse.
-     * @param projectConfiguration
-     *            A ProjectConfiguration defining the project that this connection will connect to
-     *            on map roulette
-     * @return A valid Map Roulette configuration object, null returned if the configuration string
-     *         is not valid.
-     */
-    public static MapRouletteConfiguration parseWithProject(final String configuration,
-            final ProjectConfiguration projectConfiguration)
-    {
-        if (StringUtils.isNotEmpty(configuration))
-        {
-            final String[] components = configuration.split(DELIMITER);
-            return parseWithProject(components, projectConfiguration);
-        }
-        logger.debug("Map Roulette configuration not set, invalid string passed in. [%s]",
-                configuration);
-        return null;
-    }
-
-    /**
-     * After some pre-processing has been performed, initialize a Map Roulette Configuration object
-     * from some network information and a project configuration. Note that the project
-     * configuration object also contains a project name -- if the two differ, a warning will be
-     * logged and the project name from the string array will be used.
-     *
-     * @param components
-     *            An ordered list of components defining a connection to Map Roulette. Expects the
-     *            order to be [SERVER, PORT, PROJECT_NAME, API_KEY].
-     * @param configuration
-     *            A ProjectConfiguration defining the project that this connection will connect to
-     *            on Map Roulette
-     * @return A valid Map Roulette Configuration object, null returned if the configuration string
-     *         is not valid.
-     */
-    private static MapRouletteConfiguration parseWithProject(final String[] components,
-            final ProjectConfiguration configuration)
-    {
-        if (components.length == NUMBER_OF_COMPONENTS)
-        {
-            ProjectConfiguration configurationToUse = configuration;
-            if (!configurationToUse.getName().equals(components[PROJECT_NAME_INDEX]))
-            {
-                logger.warn(
-                        "Project name from string ({}) does not equal name from configuration object ({}). Using name from string.",
-                        components[PROJECT_NAME_INDEX], configurationToUse);
-                configurationToUse = new ProjectConfiguration(components[PROJECT_NAME_INDEX],
-                        configurationToUse.getDescription(), configurationToUse.getDisplayName(),
-                        PROJECT_DEFAULT_ENABLED);
-            }
-            return new MapRouletteConfiguration(components[SERVER_INDEX],
-                    Integer.parseInt(components[PORT_INDEX]), configurationToUse,
-                    components[API_KEY_INDEX]);
-        }
-        logger.debug(String.format(
-                "Map Roulette configuration not set, invalid number of arguments. [%s]",
-                String.join(DELIMITER, components)));
         return null;
     }
 
