9039f910286e7c70f916b417f27c30a087c86879
Bentley Breithaupt <bentleysb@gmail.com>
2019-03-29 11:55:31 -0700

    AddressStreetNameCheck

---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/tag/AddressStreetNameCheckTest.java
@@ -0,0 +1,59 @@
+package org.openstreetmap.atlas.checks.validation.tag;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.atlas.checks.configuration.ConfigurationResolver;
+import org.openstreetmap.atlas.checks.validation.verifier.ConsumerBasedExpectedCheckVerifier;
+import org.openstreetmap.atlas.utilities.configuration.Configuration;
+
+/**
+ * Unit test for {@link AddressStreetNameCheck}
+ *
+ * @author bbreithaupt
+ */
+public class AddressStreetNameCheckTest
+{
+    @Rule
+    public AddressStreetNameCheckTestRule setup = new AddressStreetNameCheckTestRule();
+
+    @Rule
+    public ConsumerBasedExpectedCheckVerifier verifier = new ConsumerBasedExpectedCheckVerifier();
+
+    private final Configuration inlineConfiguration = ConfigurationResolver.inlineConfiguration(
+            "{\"InvalidLanesTagCheck\":{\"lanes.filter\":\"lanes->1,1.5,2\"}}");
+
+    @Test
+    public void validAddressStreetTagTest()
+    {
+        this.verifier.actual(this.setup.validAddressStreetTagAtlas(),
+                new AddressStreetNameCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(0, flags.size()));
+    }
+
+    @Test
+    public void validAddressStreetLocalizedTagTest()
+    {
+        this.verifier.actual(this.setup.validAddressStreetLocalizedTagAtlas(),
+                new AddressStreetNameCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(0, flags.size()));
+    }
+
+    @Test
+    public void invalidAddressStreetTagTest()
+    {
+        this.verifier.actual(this.setup.invalidAddressStreetTagAtlas(),
+                new AddressStreetNameCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(1, flags.size()));
+    }
+
+    @Test
+    public void validAddressStreetTagConfigNoEdgeInRangeTest()
+    {
+        this.verifier.actual(this.setup.validAddressStreetTagAtlas(),
+                new AddressStreetNameCheck(ConfigurationResolver
+                        .inlineConfiguration("{\"AddressStreetNameCheck.search.distance\":1.0}")));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(0, flags.size()));
+    }
+
+}
