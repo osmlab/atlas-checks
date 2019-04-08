c06f9719c939e50e715e269a98591cf23d008447
Bentley Breithaupt <bentleysb@gmail.com>
2019-04-08 11:08:08 -0700

    main logic; test framework

---
+++ src/main/java/org/openstreetmap/atlas/checks/commands/FlagSummarySubCommand.java
@@ -0,0 +1,258 @@
+package org.openstreetmap.atlas.checks.commands;
+
+import static org.openstreetmap.atlas.checks.constants.CommonConstants.COMMA;
+import static org.openstreetmap.atlas.checks.constants.CommonConstants.LINE_SEPARATOR;
+import static org.openstreetmap.atlas.geography.geojson.GeoJsonConstants.PROPERTIES;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.commons.io.FilenameUtils;
+import org.openstreetmap.atlas.streaming.resource.File;
+import org.openstreetmap.atlas.utilities.command.abstractcommand.AbstractAtlasShellToolsCommand;
+import org.openstreetmap.atlas.utilities.command.abstractcommand.CommandOutputDelegate;
+import org.openstreetmap.atlas.utilities.command.abstractcommand.OptionAndArgumentDelegate;
+import org.openstreetmap.atlas.utilities.command.parsing.OptionOptionality;
+import org.openstreetmap.atlas.utilities.scalars.Counter;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+/**
+ * @author bbreithaupt
+ */
+public class FlagSummarySubCommand extends AbstractAtlasShellToolsCommand
+{
+    private static final String INPUT_OPTION = "input";
+    private static final String UNCOMPRESSED_FLAG = "uncompressed";
+    private static final String DIFFERENCE_OPTION = "difference";
+    private static final String OUTPUT_OPTION = "output";
+    private static final String GENERATOR = "generator";
+
+    private final Gson gson = new Gson();
+    private final OptionAndArgumentDelegate optionAndArgumentDelegate;
+    private final CommandOutputDelegate outputDelegate;
+
+    public static void main(final String[] args)
+    {
+        new FlagSummarySubCommand().runSubcommandAndExit(args);
+    }
+
+    public FlagSummarySubCommand()
+    {
+        this.optionAndArgumentDelegate = this.getOptionAndArgumentDelegate();
+        this.outputDelegate = this.getCommandOutputDelegate();
+    }
+
+    @Override
+    public int execute()
+    {
+        final HashMap<String, HashMap<String, Counter>> inputCounts = this.getCountryCheckCounts(
+                this.optionAndArgumentDelegate.getOptionArgument(INPUT_OPTION).get());
+
+        final Optional<String> differenceFilePath = this.optionAndArgumentDelegate
+                .getOptionArgument(DIFFERENCE_OPTION);
+        try
+        {
+            if (differenceFilePath.isPresent())
+            {
+                final HashMap<String, HashMap<String, Counter>> targetCounts = this
+                        .getCountryCheckCounts(differenceFilePath.get());
+                this.writeOutput(getDifference(inputCounts, targetCounts),
+                        this.optionAndArgumentDelegate.getOptionArgument(OUTPUT_OPTION));
+            }
+            else
+            {
+                this.writeOutput(inputCounts,
+                        this.optionAndArgumentDelegate.getOptionArgument(OUTPUT_OPTION));
+            }
+        }
+        catch (final IOException exception)
+        {
+            this.outputDelegate.printlnStderr(exception.toString());
+            return 1;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public String getCommandName()
+    {
+        return "flag-summary";
+    }
+
+    @Override
+    public String getSimpleDescription()
+    {
+        return "get flag counts for a set of atlas checks log files";
+    }
+
+    @Override
+    public void registerManualPageSections()
+    {
+
+    }
+
+    @Override
+    public void registerOptionsAndArguments()
+    {
+        this.registerOptionWithRequiredArgument(INPUT_OPTION,
+                "A directory of folders containing atlas-checks log files.",
+                OptionOptionality.REQUIRED, INPUT_OPTION);
+        this.registerOption(UNCOMPRESSED_FLAG, 'u',
+                "Look for uncompressed log files, instead of gzipped ones.",
+                OptionOptionality.OPTIONAL);
+        this.registerOptionWithRequiredArgument(DIFFERENCE_OPTION, 'd',
+                "A second set of log files to diff against.", OptionOptionality.OPTIONAL,
+                DIFFERENCE_OPTION);
+        this.registerOptionWithRequiredArgument(OUTPUT_OPTION, 'o', "A csv to output results to.",
+                OptionOptionality.OPTIONAL, OUTPUT_OPTION);
+        super.registerOptionsAndArguments();
+    }
+
+    /**
+     * @param path
+     *            folder path
+     * @return a map
+     */
+    private HashMap<String, HashMap<String, Counter>> getCountryCheckCounts(final String path)
+    {
+        final HashMap<String, HashMap<String, Counter>> countryCheckMap = new HashMap<>();
+        new File(path).listFilesRecursively().stream()
+                .filter(file -> !file.isDirectory()
+                        && this.optionAndArgumentDelegate.hasOption(UNCOMPRESSED_FLAG) ? FilenameUtils.getExtension(file.getName()).equals("log") : file.isGzipped() && FilenameUtils.getExtension(FilenameUtils.getBaseName(file.getName())).equals("log"))
+                .forEach(file ->
+                {
+                    final String country = FilenameUtils.getName(file.getParent());
+                    countryCheckMap.putIfAbsent(country, new HashMap<>());
+
+                    try (InputStreamReader inputStreamReader = file.isGzipped()
+                            ? new InputStreamReader(
+                                    new GZIPInputStream(new FileInputStream(file.getFile())))
+                            : new FileReader(file.getPath()))
+                    {
+
+                        try (BufferedReader reader = new BufferedReader(inputStreamReader))
+                        {
+                            String line;
+                            // Read each line (flag) from the log file
+                            while ((line = reader.readLine()) != null)
+                            {
+                                // Parse the json
+                                final JsonObject source = this.gson.fromJson(line,
+                                        JsonObject.class);
+                                // Get the check name
+                                final String checkName = source.get(PROPERTIES).getAsJsonObject()
+                                        .get(GENERATOR).getAsString();
+                                countryCheckMap.get(country).putIfAbsent(checkName, new Counter());
+                                countryCheckMap.get(country).get(checkName).increment();
+                            }
+                        }
+                    }
+                    catch (final IOException exception)
+                    {
+                        this.outputDelegate.printlnWarnMessage(
+                                "File read failed with exception" + exception.getMessage());
+                    }
+                });
+        return countryCheckMap;
+    }
+
+    /**
+     * @param countryCheckCounts
+     *            map
+     * @param outputPath
+     *            file
+     * @throws IOException
+     */
+    private void writeOutput(final HashMap<String, HashMap<String, Counter>> countryCheckCounts,
+            final Optional<String> outputPath) throws IOException
+    {
+        final List<String> countries = new ArrayList<>(countryCheckCounts.keySet());
+        java.util.Collections.sort(countries);
+        final List<String> checks = countryCheckCounts.entrySet().stream()
+                .flatMap(entry -> entry.getValue().keySet().stream()).distinct().sorted()
+                .collect(Collectors.toList());
+        final StringBuilder outputString = new StringBuilder();
+
+        outputString.append("Check,");
+        outputString.append(String.join(COMMA, countries));
+        outputString.append(",Total");
+        outputString.append(LINE_SEPARATOR);
+
+        checks.forEach(check ->
+        {
+            outputString.append(check).append(COMMA);
+            outputString.append(countries.stream().map(country ->
+            {
+                if (countryCheckCounts.get(country).containsKey(check))
+                {
+                    return String.valueOf(countryCheckCounts.get(country).get(check).getValue());
+                }
+                return "ND";
+            }).collect(Collectors.joining(COMMA)));
+            outputString.append(COMMA);
+            outputString.append((Long) countries.stream()
+                    .filter(country -> countryCheckCounts.get(country).containsKey(check)).mapToLong(country -> countryCheckCounts.get(country).get(check).getValue()).sum());
+            outputString.append(LINE_SEPARATOR);
+        });
+
+        outputString.append("Total,");
+        final List<Long> countryCounts = countries.stream().map(country -> countryCheckCounts.get(country).entrySet().stream().mapToLong(entry -> entry.getValue().getValue()).sum()).collect(
+                Collectors.toList());
+        outputString.append(countryCounts.stream().map(String::valueOf).collect(Collectors.joining(COMMA)));
+        outputString.append(COMMA);
+        outputString.append(countryCounts.stream().mapToLong(Long::longValue).sum());
+
+        if (outputPath.isPresent())
+        {
+            try (FileWriter outputWriter = new FileWriter(new File(outputPath.get()).getFile()))
+            {
+                outputWriter.write(outputString.toString());
+            }
+        }
+
+        this.outputDelegate.printStdout(LINE_SEPARATOR + outputString.toString() + LINE_SEPARATOR);
+    }
+
+    /**
+     * @param source
+     *            map
+     * @param target
+     *            map
+     * @return map
+     */
+    private HashMap<String, HashMap<String, Counter>> getDifference(
+            final HashMap<String, HashMap<String, Counter>> source,
+            final HashMap<String, HashMap<String, Counter>> target)
+    {
+        final HashMap<String, HashMap<String, Counter>> difference = new HashMap<>();
+        source.forEach((country, checkCounts) ->
+        {
+            difference.putIfAbsent(country, new HashMap<>());
+            checkCounts
+                    .forEach((check, count) -> difference.get(country).put(check,
+                            new Counter(target.getOrDefault(country, new HashMap<>())
+                                    .getOrDefault(check, new Counter()).getValue()
+                                    - count.getValue())));
+        });
+        target.forEach((country, checkCounts) ->
+        {
+            difference.putIfAbsent(country, new HashMap<>());
+            checkCounts
+                    .forEach((check, count) -> difference.get(country).putIfAbsent(check, count));
+        });
+        return difference;
+    }
+}
---
+++ src/test/java/org/openstreetmap/atlas/checks/commands/FlagSummarySubCommandTest.java
@@ -0,0 +1,106 @@
+package org.openstreetmap.atlas.checks.commands;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.io.FilenameUtils;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.atlas.checks.event.CheckFlagEvent;
+import org.openstreetmap.atlas.checks.event.CheckFlagFileProcessor;
+import org.openstreetmap.atlas.checks.event.FileProcessor;
+import org.openstreetmap.atlas.checks.event.ShutdownEvent;
+import org.openstreetmap.atlas.generator.tools.spark.utilities.SparkFileHelper;
+import org.openstreetmap.atlas.streaming.resource.File;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FlagSummarySubCommandTest
+{
+    // An empty HashMap as an empty Spark configuration
+    private static final Map<String, String> FILE_SYSTEM_CONFIG = new HashMap<>();
+
+    // Temp directories
+    private static final File SOURCE_DIRECTORY = File.temporaryFolder();
+    private static final File TARGET_DIRECTORY = File.temporaryFolder();
+
+    private static final boolean[] COMPRESSION_OPTIONS = {false, true};
+
+    private static final String COUTRY_1 = "ABC";
+    private static final String COUTRY_2 = "XYZ";
+
+    private static final String CHECK_1 = "Check1";
+    private static final String CHECK_2 = "Check2";
+
+    private boolean testDataCreated = false;
+
+    @Rule
+    public final FlagSummarySubCommandTestRule setup = new FlagSummarySubCommandTestRule();
+
+    /**
+     * Generate flag files from {@link CheckFlagEvent}s, into source and target directories. The
+     * files can be compressed.
+     *
+     */
+    private void generateLogFilesForCountry(final File directory, final String country, final Map<String, Integer> checkFlagCounts)
+    {
+        final String countryFolderPath = FilenameUtils.concat(directory.getAbsolutePath(), country);
+
+        for (final boolean compression : COMPRESSION_OPTIONS){
+            final FileProcessor<CheckFlagEvent> fileProcessor = new CheckFlagFileProcessor(
+                    new SparkFileHelper(FILE_SYSTEM_CONFIG), countryFolderPath)
+                    .withCompression(compression);
+            checkFlagCounts.forEach((check, flagCount) -> {
+                for (int count = 0; count < flagCount; count++){
+                    fileProcessor.process(this.setup.getOneNodeCheckFlagEvent(check));
+                }
+            });
+            fileProcessor.process(new ShutdownEvent());
+        }
+    }
+
+    /**
+     * Generate directories of flag files and gather the path to the first file in each.
+     */
+    private void populateTestData()
+    {
+        if (!this.testDataCreated)
+        {
+            this.generateLogFilesForCountry(SOURCE_DIRECTORY, COUTRY_1, ImmutableMap.of(CHECK_1, 3, CHECK_2, 2));
+            this.generateLogFilesForCountry(SOURCE_DIRECTORY, COUTRY_2, ImmutableMap.of(CHECK_1, 2));
+            this.generateLogFilesForCountry(TARGET_DIRECTORY, COUTRY_1, ImmutableMap.of(CHECK_1, 3, CHECK_2, 1));
+            this.generateLogFilesForCountry(TARGET_DIRECTORY, COUTRY_2, ImmutableMap.of(CHECK_1, 4));
+            this.testDataCreated = true;
+        }
+    }
+
+    @AfterClass
+    public static void deleteLogFiles()
+    {
+        SOURCE_DIRECTORY.deleteRecursively();
+        TARGET_DIRECTORY.deleteRecursively();
+    }
+
+    @Test
+    public void getCountsTest() throws IOException
+    {
+        this.populateTestData();
+        final File outputFile = File.temporary();
+
+        final String[] arguments = {"--input="+SOURCE_DIRECTORY.getAbsolutePath(), "--output="+outputFile.getAbsolutePath()};
+        // FlagSummarySubCommand.main(arguments);
+
+        final String expectedText = "Check,ABC,XYZ,TotalCheck1,3,2,5Check2,2,ND,2Total,5,2,7";
+        final String actualText = new BufferedReader(new FileReader(outputFile.getFile())).lines().collect(Collectors.joining());
+
+        Assert.assertEquals(expectedText, actualText);
+
+        outputFile.delete();
+    }
+
+}
