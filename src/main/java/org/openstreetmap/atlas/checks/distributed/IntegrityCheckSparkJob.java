fdb83725b12398b81e5647df9427eba8f51fb569
Jack Klamer <jfklamer@gmail.com>
2019-06-13 16:00:21 -0700

    save work

--- src/main/java/org/openstreetmap/atlas/checks/distributed/IntegrityCheckSparkJob.java
+++ src/main/java/org/openstreetmap/atlas/checks/distributed/IntegrityCheckSparkJob.java
@@ -1,7 +1,6 @@
 package org.openstreetmap.atlas.checks.distributed;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -44,7 +43,6 @@ import org.openstreetmap.atlas.utilities.collections.StringList;
 import org.openstreetmap.atlas.utilities.configuration.Configuration;
 import org.openstreetmap.atlas.utilities.configuration.MergedConfiguration;
 import org.openstreetmap.atlas.utilities.configuration.StandardConfiguration;
-import org.openstreetmap.atlas.utilities.conversion.StringConverter;
 import org.openstreetmap.atlas.utilities.runtime.CommandMap;
 import org.openstreetmap.atlas.utilities.scalars.Duration;
 import org.openstreetmap.atlas.utilities.threads.Pool;
@@ -60,51 +58,9 @@ import scala.Tuple2;
  *
  * @author mgostintsev
  */
-public class IntegrityCheckSparkJob extends SparkJob
+public class IntegrityCheckSparkJob extends IntegrityChecksCommandArguments
 {
-    /**
-     * @author brian_l_davis
-     */
-    private enum OutputFormats
-    {
-        FLAGS,
-        GEOJSON,
-        METRICS,
-        TIPPECANOE
-    }
 
-    @Deprecated
-    protected static final Switch<String> ATLAS_FOLDER = new Switch<>("inputFolder",
-            "Path of folder which contains Atlas file(s)", StringConverter.IDENTITY,
-            Optionality.OPTIONAL);
-    // Configuration
-    private static final Switch<StringList> CONFIGURATION_FILES = new Switch<>("configFiles",
-            "Comma-separated list of configuration datasources.",
-            value -> StringList.split(value, CommonConstants.COMMA), Optionality.OPTIONAL);
-    private static final Switch<String> CONFIGURATION_JSON = new Switch<>("configJson",
-            "Json formatted configuration.", StringConverter.IDENTITY, Optionality.OPTIONAL);
-    private static final Switch<String> COUNTRIES = new Switch<>("countries",
-            "Comma-separated list of country ISO3 codes to be processed", StringConverter.IDENTITY,
-            Optionality.REQUIRED);
-    private static final Switch<MapRouletteConfiguration> MAP_ROULETTE = new Switch<>("maproulette",
-            "Map roulette server information, format <Host>:<Port>:<ProjectName>:<ApiKey>, projectName is optional.",
-            MapRouletteConfiguration::parse, Optionality.OPTIONAL);
-    private static final Switch<Rectangle> PBF_BOUNDING_BOX = new Switch<>("pbfBoundingBox",
-            "OSM protobuf data will be loaded only in this bounding box", Rectangle::forString,
-            Optionality.OPTIONAL);
-    private static final Switch<Boolean> PBF_SAVE_INTERMEDIATE_ATLAS = new Switch<>("savePbfAtlas",
-            "Saves intermediate atlas files created when processing OSM protobuf data.",
-            Boolean::valueOf, Optionality.OPTIONAL, "false");
-    private static final Switch<Set<OutputFormats>> OUTPUT_FORMATS = new Switch<>("outputFormats",
-            String.format(
-                    "Comma-separated list of output formats (flags, metrics, geojson, tippecanoe)."),
-            csv_formats -> Stream.of(csv_formats.split(","))
-                    .map(format -> Enum.valueOf(OutputFormats.class, format.toUpperCase()))
-                    .collect(Collectors.toSet()),
-            Optionality.OPTIONAL, "flags,metrics");
-    private static final Switch<List<String>> CHECK_FILTER = new Switch<>("checkFilter",
-            "Comma-separated list of checks to run",
-            checks -> Arrays.asList(checks.split(CommonConstants.COMMA)), Optionality.OPTIONAL);
 
     // Indicator key for ignored countries
     private static final String IGNORED_KEY = "Ignored";
@@ -445,23 +401,6 @@ public class IntegrityCheckSparkJob extends SparkJob
     }
 
     /**
-     * Gets the {@link AtlasDataSource} object to load the Atlas from
-     *
-     * @param sparkContext
-     *            The Spark context
-     * @param checksConfiguration
-     *            configuration for all the checks
-     * @param pbfBoundary
-     *            The pbf boundary of type {@link Rectangle}
-     * @return A {@link AtlasDataSource}
-     */
-    protected AtlasDataSource getAtlasDataSource(final Map<String, String> sparkContext,
-            final Configuration checksConfiguration, final Rectangle pbfBoundary)
-    {
-        return new AtlasDataSource(sparkContext, checksConfiguration, pbfBoundary);
-    }
-
-    /**
      * Defines all the folders to clean before a run
      *
      * @param command
@@ -479,13 +418,7 @@ public class IntegrityCheckSparkJob extends SparkJob
         return staticPaths;
     }
 
-    @Override
-    protected SwitchList switches()
-    {
-        return super.switches().with(ATLAS_FOLDER, MAP_ROULETTE, COUNTRIES, CONFIGURATION_FILES,
-                CONFIGURATION_JSON, PBF_BOUNDING_BOX, PBF_SAVE_INTERMEDIATE_ATLAS, OUTPUT_FORMATS,
-                CHECK_FILTER);
-    }
+
 
     /**
      * Basic sanity check to ensure we aren't processing an empty list of countries or integrity
---
+++ src/test/java/org/openstreetmap/atlas/checks/utility/ShardGrouperTest.java
@@ -0,0 +1,67 @@
+package org.openstreetmap.atlas.checks.utility;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openstreetmap.atlas.exception.CoreException;
+import org.openstreetmap.atlas.geography.Latitude;
+import org.openstreetmap.atlas.geography.Location;
+import org.openstreetmap.atlas.geography.Longitude;
+import org.openstreetmap.atlas.geography.Rectangle;
+import org.openstreetmap.atlas.geography.sharding.Shard;
+import org.openstreetmap.atlas.geography.sharding.SlippyTile;
+import org.openstreetmap.atlas.utilities.scalars.Distance;
+import org.openstreetmap.atlas.utilities.scalars.Surface;
+
+/**
+ * Test for {@link ShardGrouper}
+ *
+ * @author jklamer
+ */
+public class ShardGrouperTest
+{
+    private final Random random = new Random();
+
+    @Test
+    public void testRandomShards()
+    {
+        final Queue<SlippyTile> shards = new LinkedList<>();
+        SlippyTile.allTiles(3).forEach(shards::offer);
+        final Rectangle slippyTileMax = Rectangle.forCorners(
+                new Location(Latitude.degrees(-85.0511288), Longitude.MINIMUM),
+                new Location(Latitude.degrees(85.0511288), Longitude.MAXIMUM));
+        final AtomicLong atomicLong = new AtomicLong();
+        random.ints(64, 6, 10).forEach(zoomLevel ->
+        {
+            final SlippyTile original = shards.poll();
+            SlippyTile.allTiles(zoomLevel, original.bounds().contract(Distance.ONE_METER))
+                    .forEach(smallerShard ->
+                    {
+                        shards.offer(smallerShard);
+                        atomicLong.incrementAndGet();
+                    });
+        });
+
+        final List<ShardGroup> groups = new ShardGrouper(shards, 60, Distance.TEN_MILES)
+                .getGroups();
+        Assert.assertEquals(groups.size(),
+                groups.stream().map(ShardGroup::getName).distinct().count());
+        Assert.assertEquals(atomicLong.get(), groups.stream().mapToInt(ArrayList::size).sum());
+        Assert.assertEquals(slippyTileMax.surface().asMeterSquared(),
+                groups.stream().flatMap(ArrayList::stream).map(Shard::bounds)
+                        .map(Rectangle::surface).reduce(Surface::add).get().asMeterSquared(),
+                0.0);
+    }
+
+    @Test(expected = CoreException.class)
+    public void testMaxLoad()
+    {
+        new ShardGrouper(SlippyTile.allTiles(1), 1, Distance.ONE_METER).getGroups();
+    }
+}
