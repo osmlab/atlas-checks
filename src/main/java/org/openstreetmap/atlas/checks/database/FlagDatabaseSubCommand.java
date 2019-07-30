2ec5ef798adc955483271fda640c270739037382
Daniel B <dbaah@spatialdev.com>
2019-07-30 11:40:09 -0700

    Update src/main/java/org/openstreetmap/atlas/checks/database/FlagDatabaseSubCommand.java
    re: micah's suggestion
    
    Co-Authored-By: Micah Nacht <mjnacht@gmail.com>

--- src/main/java/org/openstreetmap/atlas/checks/database/FlagDatabaseSubCommand.java
+++ src/main/java/org/openstreetmap/atlas/checks/database/FlagDatabaseSubCommand.java
@@ -319,7 +319,7 @@ public class FlagDatabaseSubCommand extends AbstractAtlasShellToolsCommand
     private long getOsmIdentifier(final JsonObject properties)
     {
         return properties.get("osmid") == null ? properties.get("osmIdentifier").getAsLong()
-                : properties.get("osm_id").getAsLong();
+                : properties.get("osmid").getAsLong();
     }
 
     /**
