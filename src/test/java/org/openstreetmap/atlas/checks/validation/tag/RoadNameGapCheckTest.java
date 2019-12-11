356b8431c18f461466f0089c4c5e25a3f5f1bcd7
smaheshwaram <smaheshwaram1@gmail.com>
2019-12-10 19:45:37 -0600

    Road name gap check, adding unit tests for use cases.

---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/tag/RoadNameGapCheckTest.java
@@ -0,0 +1,55 @@
+package org.openstreetmap.atlas.checks.validation.tag;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.atlas.checks.configuration.ConfigurationResolver;
+import org.openstreetmap.atlas.checks.validation.RoadNameGapCheck;
+import org.openstreetmap.atlas.checks.validation.verifier.ConsumerBasedExpectedCheckVerifier;
+
+/**
+ * {@link org.openstreetmap.atlas.checks.validation.RoadNameGapCheck} unit tests.
+ *
+ * @author smaheshwaram
+ */
+public class RoadNameGapCheckTest
+{
+    @Rule
+    public RoadNameGapCheckTestRule setup = new RoadNameGapCheckTestRule();
+
+    @Rule
+    public ConsumerBasedExpectedCheckVerifier verifier = new ConsumerBasedExpectedCheckVerifier();
+
+    @Test
+    public void testForEdgeWithDifferentNameTag()
+    {
+        this.verifier.actual(this.setup.getEdgeWithDifferentNameTag(),
+                new RoadNameGapCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(1, flags.size()));
+    }
+
+    @Test
+    public void testForEdgeWithNoNameTag()
+    {
+        this.verifier.actual(this.setup.getEdgeWithNoNameTag(),
+                new RoadNameGapCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(1, flags.size()));
+    }
+
+    @Test
+    public void testForInvalidHighWayTag()
+    {
+        this.verifier.actual(this.setup.isInvalidHighWayTag(),
+                new RoadNameGapCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.verifyEmpty();
+    }
+  
+  @Test
+  public void testForIsJunctionNotRoundAbout()
+  {
+    this.verifier.actual(this.setup.isJunctionNotRoundAbout(),
+            new RoadNameGapCheck(ConfigurationResolver.emptyConfiguration()));
+    this.verifier.verifyEmpty();
+  }
+
+}
---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/tag/RoadNameGapCheckTestRule.java
@@ -0,0 +1,116 @@
+package org.openstreetmap.atlas.checks.validation.tag;
+
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Edge;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Node;
+
+/**
+ * Tests for {@link org.openstreetmap.atlas.checks.validation.RoadNameGapCheck}
+ *
+ * @author smaheshwaram
+ */
+public class RoadNameGapCheckTestRule extends CoreTestRule
+{
+    private static final String TEST_1 = "47.2136626201459,-122.443275382856";
+    private static final String TEST_2 = "47.2138327316739,-122.44258668766";
+    private static final String TEST_3 = "47.2136626201459,-122.441897992465";
+    private static final String TEST_4 = "47.2138114677627,-122.440990166979";
+
+    @TestAtlas(
+            // nodes
+            nodes = { @TestAtlas.Node(coordinates = @TestAtlas.Loc(value = TEST_1), id = "0"),
+                    @TestAtlas.Node(coordinates = @TestAtlas.Loc(value = TEST_2), id = "1"),
+                    @TestAtlas.Node(coordinates = @TestAtlas.Loc(value = TEST_3), id = "2"),
+                    @TestAtlas.Node(coordinates = @TestAtlas.Loc(value = TEST_4), id = "3") },
+            // edges
+            edges = {
+                    @Edge(id = "1000000000", coordinates = { @Loc(value = TEST_1),
+                            @Loc(value = TEST_2) }, tags = { "highway=motorway", "name=aroad" }),
+                    @Edge(id = "1000000001", coordinates = { @Loc(value = TEST_2),
+                            @Loc(value = TEST_3) }, tags = { "highway=MOTORWAY",
+                                    "name=Tsing Long Highway" }),
+                    @Edge(id = "1000000002", coordinates = { @Loc(value = TEST_3),
+                            @Loc(value = TEST_4) }, tags = { "highway=SECONDARY_LINK",
+                                    "name=Tsing Long Highway" }) })
+    private Atlas invalidHighWayTag;
+  
+  @TestAtlas(
+          // nodes
+          nodes = { @Node(coordinates = @Loc(value = TEST_1), id = "0"),
+                  @Node(coordinates = @Loc(value = TEST_2), id = "1"),
+                  @Node(coordinates = @Loc(value = TEST_3), id = "2"),
+                  @Node(coordinates = @Loc(value = TEST_4), id = "3") },
+          // edges
+          edges = {
+                  @Edge(id = "1000000000", coordinates = { @Loc(value = TEST_1),
+                          @Loc(value = TEST_2) }, tags = { "highway=PRIMARY",
+                          "name=Tsing Long Highway" }),
+                  @Edge(id = "1000000001", coordinates = { @Loc(value = TEST_2),
+                          @Loc(value = TEST_3) }, tags = { "highway=MOTORWAY",
+                          "name=failingName", "junction=roundabout" }),
+                  @Edge(id = "1000000002", coordinates = { @Loc(value = TEST_3),
+                          @Loc(value = TEST_4) }, tags = { "highway=SECONDARY",
+                          "name=Tsing Long Highway" }) })
+  private Atlas junctionNotRoundAbout;
+
+    @TestAtlas(
+            // nodes
+            nodes = { @Node(coordinates = @Loc(value = TEST_1), id = "0"),
+                    @Node(coordinates = @Loc(value = TEST_2), id = "1"),
+                    @Node(coordinates = @Loc(value = TEST_3), id = "2"),
+                    @Node(coordinates = @Loc(value = TEST_4), id = "3") },
+            // edges
+            edges = {
+                    @Edge(id = "1000000000", coordinates = { @Loc(value = TEST_1),
+                            @Loc(value = TEST_2) }, tags = { "highway=PRIMARY",
+                                    "name=Tsing Long Highway" }),
+                    @Edge(id = "1000000001", coordinates = { @Loc(value = TEST_2),
+                            @Loc(value = TEST_3) }, tags = { "highway=MOTORWAY",
+                                    "name=failingName" }),
+                    @Edge(id = "1000000002", coordinates = { @Loc(value = TEST_3),
+                            @Loc(value = TEST_4) }, tags = { "highway=SECONDARY",
+                                    "name=Tsing Long Highway" }) })
+    private Atlas edgeWithDifferentNameTag;
+
+    @TestAtlas(
+            // nodes
+            nodes = { @Node(coordinates = @Loc(value = TEST_1), id = "0"),
+                    @Node(coordinates = @Loc(value = TEST_2), id = "1"),
+                    @Node(coordinates = @Loc(value = TEST_3), id = "2"),
+                    @Node(coordinates = @Loc(value = TEST_4), id = "3") },
+            // edges
+            edges = {
+                    @Edge(id = "1000000000", coordinates = { @Loc(value = TEST_1),
+                            @Loc(value = TEST_2) }, tags = { "highway=PRIMARY",
+                                    "name=Tsing Long Highway" }),
+                    @Edge(id = "1000000001", coordinates = { @Loc(value = TEST_2),
+                            @Loc(value = TEST_3) }, tags = { "highway=MOTORWAY" }),
+                    @Edge(id = "1000000002", coordinates = { @Loc(value = TEST_3),
+                            @Loc(value = TEST_4) }, tags = { "highway=SECONDARY",
+                                    "name=Tsing Long Highway" }) })
+    private Atlas edgeWithNoNameTag;
+
+    public Atlas getEdgeWithDifferentNameTag()
+    {
+        return this.edgeWithDifferentNameTag;
+    }
+
+    public Atlas getEdgeWithNoNameTag()
+    {
+        return this.edgeWithNoNameTag;
+    }
+
+    public Atlas isInvalidHighWayTag()
+    {
+        return this.invalidHighWayTag;
+    }
+  
+  public Atlas isJunctionNotRoundAbout()
+  {
+    return this.junctionNotRoundAbout;
+  }
+
+}
