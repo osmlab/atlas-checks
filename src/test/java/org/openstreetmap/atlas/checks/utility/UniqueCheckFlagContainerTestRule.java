c6ef37538923e85bb7533a1ce76b98555656d25a
Bentley Breithaupt <bentleysb@gmail.com>
2020-01-29 11:01:09 -0800

    test rule

--- src/test/java/org/openstreetmap/atlas/checks/utility/UniqueCheckFlagContainerTestRule.java
+++ src/test/java/org/openstreetmap/atlas/checks/utility/UniqueCheckFlagContainerTestRule.java
@@ -1,4 +1,28 @@
 package org.openstreetmap.atlas.checks.utility;
 
-public class UniqueCheckFlagContainerTestRule {
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Edge;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Node;
+
+public class UniqueCheckFlagContainerTestRule extends CoreTestRule
+{
+    private static final String TEST_1 = "47.8586236, -122.6417178";
+    private static final String TEST_2 = "47.8598930, -122.6398508";
+
+    @TestAtlas(
+            // nodes
+            nodes = { @Node(id = "1000000", coordinates = @Loc(value = TEST_1)),
+                    @Node(id = "2000000", coordinates = @Loc(value = TEST_2)) },
+            // edges
+            edges = { @Edge(id = "1000000", coordinates = { @Loc(value = TEST_1),
+                    @Loc(value = TEST_2) }) })
+    private Atlas atlas;
+
+    public Atlas atlas()
+    {
+        return this.atlas;
+    }
 }
