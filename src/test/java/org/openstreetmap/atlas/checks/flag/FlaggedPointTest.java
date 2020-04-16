ecedc2cbcc7411ec6e295665bd20d8782802e624
Daniel Baah <danielduhh@gmail.com>
2020-04-15 20:16:47 -0700

    add test

---
+++ src/test/java/org/openstreetmap/atlas/checks/flag/FlaggedPointTest.java
@@ -0,0 +1,22 @@
+package org.openstreetmap.atlas.checks.flag;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openstreetmap.atlas.geography.Location;
+
+public class FlaggedPointTest
+{
+    @Test
+    public void flaggedPointSyntheticTagTest()
+    {
+        final FlaggedPoint flaggedPoint = new FlaggedPoint(Location.EIFFEL_TOWER);
+        final Map<String, String> flaggedPointProperties = flaggedPoint.getProperties();
+
+        Assert.assertFalse(flaggedPointProperties.isEmpty());
+        Assert.assertTrue(flaggedPointProperties.containsKey(FlaggedPoint.SYNTHETIC_POINT_TAG));
+        Assert.assertEquals("yes", flaggedPointProperties.get(FlaggedPoint.SYNTHETIC_POINT_TAG));
+
+    }
+}
