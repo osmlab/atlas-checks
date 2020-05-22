fbedbcb29ccaf9e303e63e2c82f29e56efc48192
Bentley Breithaupt <bentleysb@gmail.com>
2020-05-22 16:17:05 -0700

    InvalidGeometryCheck

--- src/main/java/org/openstreetmap/atlas/checks/validation/intersections/SelfIntersectingPolylineCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/intersections/SelfIntersectingPolylineCheck.java
@@ -12,7 +12,6 @@ import org.openstreetmap.atlas.checks.validation.GeometryValidator;
 import org.openstreetmap.atlas.exception.CoreException;
 import org.openstreetmap.atlas.geography.Location;
 import org.openstreetmap.atlas.geography.PolyLine;
-import org.openstreetmap.atlas.geography.Polygon;
 import org.openstreetmap.atlas.geography.Segment;
 import org.openstreetmap.atlas.geography.atlas.items.Area;
 import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
@@ -143,11 +142,11 @@ public class SelfIntersectingPolylineCheck extends BaseCheck<Long>
             {
                 if (object instanceof Area)
                 {
-                    isJtsValid = GeometryValidator.isValidPolygon((Polygon) polyline);
+                    isJtsValid = GeometryValidator.testSimplicity(polyline).isEmpty();
                 }
                 else
                 {
-                    isJtsValid = GeometryValidator.isValidPolyLine(polyline);
+                    isJtsValid = GeometryValidator.testSimplicity(polyline).isEmpty();
                 }
             }
             catch (final IllegalArgumentException e)
