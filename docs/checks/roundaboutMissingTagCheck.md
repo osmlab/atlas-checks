e2daf66ff33cbbddc1573b90dcc508d0aa84424c
Vladimir Lemberg <62575608+vladlemberg@users.noreply.github.com>
2020-06-24 16:28:55 -0700

    Vlad missing roundabout tag (#315)
    * fix traversing via reversed edges.
    
    * fix traversing via reversed edges.
    
    * fix traversing via reversed edges.
    
    * adding original OSM identifier for convenient debugging
    
    * Atlas new check for roundabout missing tag
    
    * Atlas new check for roundabout missing tag
    
    * Fixed Style issues
    
    * Fixed Style issues
    
    * Fixed Style issues
    
    * Merge latest changes from dev branch. And enable default check run
    
    * Fixed Style issues
    
    * Fixed Style issues
    
    * Fixed spotless check
    
    * Fixed spotless check
    
    * merging if statements
    
    * adding more unit test coverage
    
    * adding more unit test coverage
    
    * minor correction
    
    * add MD doc file and check description in available checks.
    
    * fix style issue
    
    * added code review suggestions.
    
    * added code review suggestions.

---
+++ docs/checks/roundaboutMissingTagCheck.md
@@ -0,0 +1,38 @@
+# Roundabout Missing Tag Check
+
+#### Description
+This check identifies closed navigable ways https://wiki.openstreetmap.org/wiki/Item:Q4669 
+that have a round shape and intersecting with at least two navigable ways.    
+
+#### Live Example
+Roundabout without `junction=roundabout` tag.
+1) [id:592687817](https://www.openstreetmap.org/way/592687817) 
+2) [id:548529232](https://www.openstreetmap.org/way/548529232)
+3) [id:150664424](https://www.openstreetmap.org/way/150664424)
+4) [id:287957529](https://www.openstreetmap.org/way/287957529)
+
+#### Code Review
+In [Atlas](https://github.com/osmlab/atlas), OSM elements are represented as Edges, Points, Lines, 
+Nodes & Relations; in our case, we’re working with [Edges](https://github.com/osmlab/atlas/blob/dev/src/main/java/org/openstreetmap/atlas/geography/atlas/items/Edge.java).
+In OpenStreetMap, roundabouts are [Ways](https://wiki.openstreetmap.org/wiki/Way) classified with
+the `junction=roundabout` tag. We’ll identify ways that are roundabouts without `junction=roundabout` tag.
+
+Our first goal is to validate the incoming Atlas Object.
+* Must be a first section of valid master Edge
+* Must have not already been flagged
+* Must be car navigable
+* Must have not `junction` and `area` tag
+* Must be a closed way [OSM-wiki:Closed Way](https://wiki.openstreetmap.org/wiki/Item:Q4669)
+* Must intersect at minimum with two navigable ways 
+
+After the preliminary filtering of features we need to rebuild original OSM way geometry.
+Ones we have rebuilt the geometry, we will measure angles between each [Nodes](https://wiki.openstreetmap.org/wiki/Node)
+ensuring that roundabout candidate is round shaped within a threshold.   
+
+We use a
+[OsmWayWalker](https://github.com/osmlab/atlas/blob/dev/src/main/java/org/openstreetmap/atlas/geography/atlas/walker/OsmWayWalker.java)
+to gather all sectioned Edges that are connected to the original Edge. 
+
+
+To learn more about the code, please look at the comments in the source code for the check.
+[RoundaboutMissingTag.java](../../src/main/java/org/openstreetmap/atlas/checks/validation/linear/edges/RoundaboutMissingTagCheck.java)
---
+++ src/main/java/org/openstreetmap/atlas/checks/validation/linear/edges/RoundaboutMissingTagCheck.java
@@ -0,0 +1,170 @@
+package org.openstreetmap.atlas.checks.validation.linear.edges;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.openstreetmap.atlas.checks.base.BaseCheck;
+import org.openstreetmap.atlas.checks.flag.CheckFlag;
+import org.openstreetmap.atlas.geography.Location;
+import org.openstreetmap.atlas.geography.PolyLine;
+import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
+import org.openstreetmap.atlas.geography.atlas.items.Edge;
+import org.openstreetmap.atlas.geography.atlas.walker.OsmWayWalker;
+import org.openstreetmap.atlas.tags.AreaTag;
+import org.openstreetmap.atlas.tags.HighwayTag;
+import org.openstreetmap.atlas.tags.JunctionTag;
+import org.openstreetmap.atlas.tags.ServiceTag;
+import org.openstreetmap.atlas.utilities.configuration.Configuration;
+import org.openstreetmap.atlas.utilities.scalars.Angle;
+import org.openstreetmap.atlas.utilities.tuples.Tuple;
+
+/**
+ * Flags edges that are closed and round shaped without junction=roundabout tag and have minimum of
+ * two intersections with navigable roads {@link RoundaboutMissingTagCheck#MINIMUM_INTERSECTION}
+ * connections. See https://wiki.openstreetmap.org/wiki/Tag:junction%3Droundabout for more
+ * information about roundabouts
+ * 
+ * @author vladlemberg
+ */
+
+public class RoundaboutMissingTagCheck extends BaseCheck<Long>
+{
+    // Instructions
+    public static final String MISSING_JUNCTION_TAG_INSTRUCTION = "This edge might be a roundabout";
+    // Minimum intersection with Navigable Roads
+    private static final int MINIMUM_INTERSECTION = 2;
+    private static final int MODULUS = 10;
+    private static final int FIRST_EDGE_SECTION = 1;
+    private static final List<String> FALLBACK_INSTRUCTIONS = Collections
+            .singletonList(MISSING_JUNCTION_TAG_INSTRUCTION);
+    private static final double MAX_THRESHOLD_DEGREES_DEFAULT = 40.0;
+    private static final double MIN_THRESHOLD_DEGREES_DEFAULT = 10.0;
+    private final Angle maxAngleThreshold;
+    private final Angle minAngleThreshold;
+
+    /**
+     * Default constructor
+     *
+     * @param configuration
+     *            the JSON configuration for this check
+     */
+    public RoundaboutMissingTagCheck(final Configuration configuration)
+    {
+        super(configuration);
+        this.maxAngleThreshold = configurationValue(configuration, "angle.threshold.maximum_degree",
+                MAX_THRESHOLD_DEGREES_DEFAULT, Angle::degrees);
+        this.minAngleThreshold = configurationValue(configuration, "angle.threshold.minimum_degree",
+                MIN_THRESHOLD_DEGREES_DEFAULT, Angle::degrees);
+    }
+
+    /**
+     * Validates if given {@link AtlasObject} is actually an {@link Edge} and is a potential
+     * roundabout and also corresponding OSM identifier shouldn't be flagged before (this is for
+     * avoiding duplicate flags)
+     */
+    @Override
+    public boolean validCheckForObject(final AtlasObject object)
+    {
+        return object instanceof Edge && !this.isFlagged(object.getOsmIdentifier())
+                && object.getIdentifier() % MODULUS == FIRST_EDGE_SECTION
+                && ((Edge) object).isMasterEdge() && HighwayTag.isCarNavigableHighway(object)
+                && object.getTag(JunctionTag.KEY).isEmpty() && object.getTag(AreaTag.KEY).isEmpty()
+                && this.isPartOfClosedWay((Edge) object)
+                && this.intersectingWithMoreThan((Edge) object);
+    }
+
+    /**
+     * Flags an {@link Edge} is its circular shape and connected to at least
+     * {@link RoundaboutMissingTagCheck#MINIMUM_INTERSECTION} navigable roads. {@link Edge} doesn't
+     * have junction=roundabout or area=yes tags.
+     */
+    @Override
+    protected Optional<CheckFlag> flag(final AtlasObject object)
+    {
+        final Edge edge = (Edge) object;
+
+        final PolyLine originalGeom = buildOriginalOsmWayGeometry(edge);
+        // check maximum angle
+        final List<Tuple<Angle, Location>> maxOffendingAngles = originalGeom
+                .anglesGreaterThanOrEqualTo(this.maxAngleThreshold);
+        // check minimum angle
+        final List<Tuple<Angle, Location>> minOffendingAngles = originalGeom
+                .anglesLessThanOrEqualTo(this.minAngleThreshold);
+
+        if (maxOffendingAngles.isEmpty() && minOffendingAngles.isEmpty())
+        {
+            this.markAsFlagged(object.getOsmIdentifier());
+            return Optional.of(createFlag(new OsmWayWalker(edge).collectEdges(),
+                    this.getLocalizedInstruction(0)));
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    protected List<String> getFallbackInstructions()
+    {
+        return FALLBACK_INSTRUCTIONS;
+    }
+
+    /**
+     * Build original OSW way geometry from all MasterEdge sections
+     *
+     * @param edge
+     *            entity to check
+     * @return original Way geometry polyline
+     */
+    private PolyLine buildOriginalOsmWayGeometry(final Edge edge)
+    {
+        // Identify and sort by IDs all sections of original OSM way
+        final List<Edge> sortedEdges = new ArrayList<>(new OsmWayWalker(edge).collectEdges());
+        // Build original OSM polyline
+        PolyLine geometry = new PolyLine(sortedEdges.get(0).getRawGeometry());
+        for (int index = 1; index < sortedEdges.size(); index++)
+        {
+            geometry = geometry.append(sortedEdges.get(index).asPolyLine());
+        }
+        return geometry;
+    }
+
+    /**
+     * Check if original OSM Way is intersecting with CAR_NAVIGABLE_HIGHWAYS. See {@link HighwayTag}
+     *
+     * @param edge
+     *            entity to check
+     * @return true if way intersecting with more more than
+     *         {@link RoundaboutMissingTagCheck#MINIMUM_INTERSECTION}
+     */
+    private boolean intersectingWithMoreThan(final Edge edge)
+    {
+        // Identify all sections of original OSM way
+        final Set<Edge> edgesFormingOSMWay = new HashSet<>(new OsmWayWalker(edge).collectEdges());
+        final Set<Long> connectedEdges = new HashSet<>();
+        edgesFormingOSMWay.forEach(
+                connectedEdge -> connectedEdge.connectedEdges().stream().filter(Edge::isMasterEdge)
+                        // intersection with navigable roads
+                        .filter(HighwayTag::isCarNavigableHighway)
+                        .filter(obj -> obj.getTag(ServiceTag.KEY).isEmpty())
+                        // de-duplication sectioned edges
+                        .forEach(wayId -> connectedEdges.add(wayId.getOsmIdentifier())));
+        return connectedEdges.size() > MINIMUM_INTERSECTION;
+    }
+
+    /**
+     * Check if Edge is part of Closed Way. See https://wiki.openstreetmap.org/wiki/Item:Q4669
+     *
+     * @param edge
+     *            entity to check
+     * @return true if edge is part of closed way.
+     */
+    private boolean isPartOfClosedWay(final Edge edge)
+    {
+        return edge.inEdges().stream().filter(Edge::isMasterEdge)
+                .filter(inEdge -> inEdge.getOsmIdentifier() == edge.getOsmIdentifier())
+                .count() == 1;
+    }
+}
