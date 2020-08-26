36b08d5656fcfc889017663b89aa0c609b006783
Taylor Smock <taylor.smock@kaart.com>
2020-08-26 14:25:02 -0600

    Improve check (ignore dams, better intersection handling)
    Signed-off-by: Taylor Smock <taylor.smock@kaart.com>

--- src/main/java/org/openstreetmap/atlas/checks/validation/areas/WaterAreaCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/areas/WaterAreaCheck.java
@@ -2,15 +2,19 @@ package org.openstreetmap.atlas.checks.validation.areas;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.openstreetmap.atlas.checks.base.BaseCheck;
 import org.openstreetmap.atlas.checks.flag.CheckFlag;
 import org.openstreetmap.atlas.checks.utility.IntersectionUtilities;
+import org.openstreetmap.atlas.geography.Location;
+import org.openstreetmap.atlas.geography.PolyLine;
 import org.openstreetmap.atlas.geography.Polygon;
 import org.openstreetmap.atlas.geography.Segment;
 import org.openstreetmap.atlas.geography.atlas.items.Area;
@@ -22,7 +26,7 @@ import org.openstreetmap.atlas.utilities.collections.Iterables;
 import org.openstreetmap.atlas.utilities.configuration.Configuration;
 
 /**
- * This checks water areas for overlaps and missing waterways (where appropriate)
+ * This checks water areas for overlaps and missing waterways (where appropriate).
  *
  * @author Taylor Smock
  */
@@ -35,6 +39,11 @@ public class WaterAreaCheck extends BaseCheck<Long>
     private static final List<String> WATER_FILTERS_WATERWAY = Arrays
             .asList("natural->water&water->river,stream_pool,canal,lock|waterway->riverbank");
     private static final List<String> WATERWAY_FILTERS = Arrays.asList("waterway->*");
+
+    // https://wiki.openstreetmap.org/wiki/Tag:waterway%3Ddam specifies that dams
+    // may cross other waterways
+    // Probably should not cross other waterways with the same tag though.
+    private static final List<String> WATERWAY_CROSSING_IGNORE = Arrays.asList("waterway->dam");
 
     private static final List<String> FALLBACK_INSTRUCTIONS = Arrays.asList(
             "Waterway area (id={0,number,#}) is missing a waterway way.",
@@ -51,6 +60,7 @@ public class WaterAreaCheck extends BaseCheck<Long>
     private final List<TaggableFilter> areaFilters = new ArrayList<>();
     private final List<TaggableFilter> waterRequiringWaterwayFilters = new ArrayList<>();
     private final List<TaggableFilter> waterwayFilters = new ArrayList<>();
+    private final List<TaggableFilter> waterwayCrossingIgnore = new ArrayList<>();
 
     public static boolean matchesFilter(final List<TaggableFilter> filters,
             final AtlasObject object)
@@ -80,6 +90,10 @@ public class WaterAreaCheck extends BaseCheck<Long>
                 WATER_FILTERS_WATERWAY);
         filtersString.forEach(string -> this.waterRequiringWaterwayFilters
                 .add(TaggableFilter.forDefinition(string)));
+        filtersString = configurationValue(configuration, "water.tags.crossing.ignore",
+                WATERWAY_CROSSING_IGNORE);
+        filtersString.forEach(
+                string -> this.waterwayCrossingIgnore.add(TaggableFilter.forDefinition(string)));
     }
 
     @Override
@@ -93,38 +107,18 @@ public class WaterAreaCheck extends BaseCheck<Long>
     protected Optional<CheckFlag> flag(final AtlasObject object)
     {
         final Area area = (Area) object;
-        final Polygon areaPolygon = area.asPolygon();
-        final List<Area> intersections = Iterables
-                .stream(area.getAtlas().areasIntersecting(areaPolygon,
-                        o -> matchesSameFilter(this.areaFilters, object, o)
-                                && IntersectionUtilities.findIntersectionPercentage(areaPolygon,
-                                        o.asPolygon()) >= this.minimumIntersect
-                                && !object.equals(o)))
-                .collectToList();
+        final Polygon areaPolygon = area.getClosedGeometry();
         final List<Line> waterways = Iterables.stream(area.getAtlas().linesIntersecting(areaPolygon,
                 o -> matchesFilter(this.waterwayFilters, o))).collectToList();
         final CheckFlag flag = new CheckFlag(this.getTaskIdentifier(object));
         flag.addObject(object);
         if (waterways.isEmpty() && matchesFilter(this.waterRequiringWaterwayFilters, object))
         {
-            // TODO add appropriate instruction
             flag.addInstruction(this.getLocalizedInstruction(0, object.getOsmIdentifier()));
             this.markAsFlagged(object.getOsmIdentifier());
             return Optional.of(flag);
         }
-        else if (!intersections.isEmpty())
-        {
-            intersections.forEach(flag::addObject);
-            this.markAsFlagged(object.getOsmIdentifier());
-            intersections.stream().map(AtlasObject::getOsmIdentifier).forEach(this::markAsFlagged);
-            flag.addInstruction(
-                    "Intersections exist: " + object.getOsmIdentifier() + " intersects with "
-                            + String.join(", ",
-                                    intersections.stream().map(AtlasObject::getOsmIdentifier)
-                                            .map(Object::toString).collect(Collectors.toList())));
-            return Optional.of(flag);
-        }
-        else if (!waterways.isEmpty())
+        if (!waterways.isEmpty())
         {
             final List<Segment> areaSegments = areaPolygon
                     .segments().stream().filter(s -> waterways.parallelStream()
@@ -132,49 +126,59 @@ public class WaterAreaCheck extends BaseCheck<Long>
                     .collect(Collectors.toList());
             if (areaSegments.isEmpty())
             {
-                // TODO add appropriate instruction
                 flag.addInstruction(this.getLocalizedInstruction(1, object.getOsmIdentifier()));
                 this.markAsFlagged(object.getOsmIdentifier());
                 return Optional.of(flag);
             }
-            else
-            {
-                final List<Pair<Segment, List<Area>>> possibleAreaIntersections = areaSegments
-                        .stream().map(
-                                b -> Pair.of(b,
-                                        Iterables
-                                                .stream(object.getAtlas().areasIntersecting(
-                                                        b.bounds(),
-                                                        o -> matchesFilter(this.waterwayFilters, o)
-                                                                && !object.equals(o)
-                                                                && o.asPolygon()
-                                                                        .overlaps(areaPolygon)))
-                                                .collectToList()))
-                        .filter(p -> !p.getRight().isEmpty()).collect(Collectors.toList());
+        }
+        final List<Pair<Segment, List<Area>>> possibleAreaIntersections = area.getClosedGeometry()
+                .segments().stream()
+                .map(b -> Pair.of(b,
+                        Iterables
+                                .stream(object.getAtlas().areasIntersecting(b.bounds(),
+                                        o -> matchesFilter(this.areaFilters, o) && !object.equals(o)
+                                                && area.getClosedGeometry()
+                                                        .intersects(o.getClosedGeometry())))
+                                .collectToList()))
+                .filter(p -> !p.getRight().isEmpty()).collect(Collectors.toList());
 
-                final List<Area> areaIntersections = possibleAreaIntersections.stream()
-                        .flatMap(p -> p.getRight().stream()
-                                .filter(a -> a.asPolygon().segments().stream()
-                                        .noneMatch(s -> p.getLeft().equals(s)
-                                                || (s.start().equals(p.getLeft().end())
-                                                        && s.end().equals(p.getLeft().start())))))
-                        .collect(Collectors.toList());
-                if (!areaIntersections.isEmpty() && !alreadyFlagged(areaIntersections))
-                {
-                    flag.addPoints(possibleAreaIntersections.stream()
-                            .filter(p -> !alreadyFlagged(p.getRight())).map(Pair::getLeft)
-                            .map(Segment::middle).collect(Collectors.toList()));
-                    // TODO add appropriate instruction
-                    flag.addInstruction(this.getLocalizedInstruction(2, object.getOsmIdentifier(),
-                            String.join(", ",
-                                    areaIntersections.stream().map(AtlasObject::getOsmIdentifier)
-                                            .distinct().map(Objects::toString)
-                                            .collect(Collectors.toList()))));
-                    areaIntersections.forEach(flag::addObject);
-                    this.markAsFlagged(object.getOsmIdentifier());
-                    return Optional.of(flag);
-                }
-            }
+        final List<Area> areaIntersections = possibleAreaIntersections.stream()
+                .flatMap(p -> p.getRight().stream()).distinct()
+                .filter(a -> !intersections(area.getClosedGeometry(), a.getClosedGeometry())
+                        .isEmpty())
+                .filter(a -> matchesFilter(this.waterwayCrossingIgnore, a)
+                        && matchesFilter(this.waterwayCrossingIgnore, area)
+                        || !matchesFilter(this.waterwayCrossingIgnore, a)
+                                && !matchesFilter(this.waterwayCrossingIgnore, area))
+                .collect(Collectors.toList());
+        if (!areaIntersections.isEmpty() && !alreadyFlagged(areaIntersections))
+        {
+            flag.addPoints(
+                    possibleAreaIntersections.stream().filter(p -> !alreadyFlagged(p.getRight()))
+                            .map(Pair::getLeft).map(Segment::middle).collect(Collectors.toList()));
+            flag.addInstruction(this.getLocalizedInstruction(2, object.getOsmIdentifier(),
+                    areaIntersections.stream().map(AtlasObject::getOsmIdentifier).distinct()
+                            .map(Objects::toString).collect(Collectors.joining(", "))));
+            areaIntersections.forEach(flag::addObject);
+            this.markAsFlagged(object.getOsmIdentifier());
+            return Optional.of(flag);
+        }
+
+        // Sometimes there will be two waterways that share every exterior intersection,
+        // but one or the other cuts a corner somewhere.
+        final List<Area> areaOverlaps = possibleAreaIntersections.stream()
+                .flatMap(p -> p.getRight().stream()).distinct()
+                .filter(a -> IntersectionUtilities.findIntersectionPercentage(a.getClosedGeometry(),
+                        area.getClosedGeometry()) >= this.minimumIntersect)
+                .collect(Collectors.toList());
+        if (!areaOverlaps.isEmpty())
+        {
+            flag.addInstruction(this.getLocalizedInstruction(2, object.getOsmIdentifier(),
+                    areaOverlaps.stream().map(AtlasObject::getOsmIdentifier).distinct()
+                            .map(Objects::toString).collect(Collectors.joining(", "))));
+            areaOverlaps.forEach(flag::addObject);
+            this.markAsFlagged(object.getOsmIdentifier());
+            return Optional.of(flag);
         }
         return Optional.empty();
     }
@@ -189,5 +193,31 @@ public class WaterAreaCheck extends BaseCheck<Long>
     {
         return this.getFlaggedIdentifiers().containsAll(objects.parallelStream()
                 .map(AtlasObject::getOsmIdentifier).collect(Collectors.toList()));
+    }
+
+    /**
+     * Get the intersections between two polylines. Unlike {@link PolyLine#intersections}, this does
+     * not include points that are shared between the two lines.
+     *
+     * @param line1
+     *            A line to check for intersections
+     * @param line2
+     *            A line to check for intersections
+     * @return The intersections of line1 and line2 where line1 and line2 are not connected.
+     */
+    private Set<Location> intersections(final PolyLine line1, final PolyLine line2)
+    {
+        // An intersection is any shared point OR overlap
+        if (line1.intersects(line2))
+        {
+            final Set<Location> intersections = line1.intersections(line2);
+            // Remove intersections that are points on both lines
+            intersections.removeIf(l -> line1.contains(l) && line2.contains(l));
+            if (!intersections.isEmpty())
+            {
+                return intersections;
+            }
+        }
+        return Collections.emptySet();
     }
 }
