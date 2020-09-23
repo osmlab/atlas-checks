91b7ec1c17a650fd005acd201df0fe942419e6ea
Bentley Breithaupt <bentleysb@gmail.com>
2020-09-23 14:40:22 -0700

    CheckFlag Fix Suggestions (#361)
    * fix suggestions
    
    * fix tag
    
    * comment and constant
    
    * comment and constant

--- src/test/java/org/openstreetmap/atlas/checks/validation/areas/AreasWithHighwayTagCheckTest.java
+++ src/test/java/org/openstreetmap/atlas/checks/validation/areas/AreasWithHighwayTagCheckTest.java
@@ -1,14 +1,19 @@
 package org.openstreetmap.atlas.checks.validation.areas;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openstreetmap.atlas.checks.configuration.ConfigurationResolver;
+import org.openstreetmap.atlas.checks.flag.CheckFlag;
 import org.openstreetmap.atlas.checks.validation.verifier.ConsumerBasedExpectedCheckVerifier;
 
 /**
  * @author matthieun
  * @author daniel-baah
+ * @author bbreithaupt
  */
 public class AreasWithHighwayTagCheckTest
 {
@@ -20,6 +25,17 @@ public class AreasWithHighwayTagCheckTest
     private final AreasWithHighwayTagCheck check = new AreasWithHighwayTagCheck(
             ConfigurationResolver.inlineConfiguration(
                     "{\"AreasWithHighwayTagCheck\":{\"tags.filter\":\"highway->*&area->yes\"}}"));
+
+    private static void verifyObjectsAndSuggestions(final CheckFlag flag, final int count)
+    {
+        Assert.assertEquals(count, flag.getFlaggedObjects().size());
+        Assert.assertEquals(count, flag.getFixSuggestions().size());
+        final List<Long> objectIds = flag.getFlaggedObjects().stream()
+                .map(object -> Long.valueOf(object.getProperties().get("identifier")))
+                .collect(Collectors.toList());
+        flag.getFixSuggestions().forEach(
+                suggestion -> Assert.assertTrue(objectIds.contains(suggestion.getIdentifier())));
+    }
 
     @Test
     public void areaNoHighwayTag()
@@ -33,21 +49,23 @@ public class AreasWithHighwayTagCheckTest
     {
         this.verifier.actual(this.setup.connectedEdgesBadTags(), this.check);
         this.verifier.verifyExpectedSize(1);
-        this.verifier.verify(flag -> Assert.assertEquals(2, flag.getFlaggedObjects().size()));
+        this.verifier.verify(flag -> verifyObjectsAndSuggestions(flag, 2));
     }
 
     @Test
     public void invalidAreaHighwayFootwayTag()
     {
         this.verifier.actual(this.setup.invalidAreaHighwayFootwayTagAtlas(), this.check);
-        this.verifier.verifyNotEmpty();
+        this.verifier.verifyExpectedSize(1);
+        this.verifier.verify(flag -> verifyObjectsAndSuggestions(flag, 1));
     }
 
     @Test
     public void invalidAreaHighwayPrimaryTag()
     {
         this.verifier.actual(this.setup.invalidAreaHighwayPrimaryTagAtlas(), this.check);
-        this.verifier.verifyNotEmpty();
+        this.verifier.verifyExpectedSize(1);
+        this.verifier.verify(flag -> verifyObjectsAndSuggestions(flag, 1));
     }
 
     @Test
@@ -55,6 +73,7 @@ public class AreasWithHighwayTagCheckTest
     {
         this.verifier.actual(this.setup.invalidEdgeHighwaySecondary(), this.check);
         this.verifier.verifyExpectedSize(1);
+        this.verifier.verify(flag -> verifyObjectsAndSuggestions(flag, 1));
     }
 
     @Test
