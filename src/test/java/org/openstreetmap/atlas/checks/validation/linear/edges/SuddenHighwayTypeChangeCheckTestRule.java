05112bdeaadff621f6aa6073ce27cea976514da2
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2020-10-23 13:42:02 -0700

    Suddenhighwaytypechange (#381)
    * new suddenhighwaytypechangecheck with simplified logic and more inline with osmose issue 1090.
    
    * Updated check and required documents
    
    * Fixed test issues
    
    * Replacing strings with HighwayType.
    
    * removing config default value
    
    * updating documentation regarding the suddenhighwaytypechangecheck
    
    * spotless apply
    
    * fixing sonar cloud errors and spotless
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * consolidated conditionals to simplify final method
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * removing marking connected edges as flagged to maximize detections.
    
    * Update SuddenHighwayTypeChangeCheck.java
    
    * fixing change requests from pull request.
    
    * updated test checks for cases and addressed comments
    
    * fixed config
    
    * fixing code smells
    
    * hopefully last spotless apply
    
    * fixed config
    
    * siplified the main functions with return true or false

---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/linear/edges/SuddenHighwayTypeChangeCheckTestRule.java
@@ -0,0 +1,91 @@
+package org.openstreetmap.atlas.checks.validation.linear.edges;
+
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Edge;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Node;
+
+/**
+ * Tests for {@link SuddenHighwayTypeChangeCheck}
+ *
+ * @author v-garei
+ */
+public class SuddenHighwayTypeChangeCheckTestRule extends CoreTestRule
+{
+
+    private static final String WAY1_NODE1 = "47.6317191, -122.1961930";
+    private static final String WAY1_WAY2_INTERSECTION = "47.6316431, -122.1962111";
+    private static final String WAY2_NODE2 = "47.6315832, -122.1961171";
+
+    @TestAtlas(nodes = { @Node(coordinates = @Loc(value = WAY1_NODE1)),
+            @Node(coordinates = @Loc(value = WAY1_WAY2_INTERSECTION)),
+            @Node(coordinates = @Loc(value = WAY2_NODE2)) }, edges = {
+                    @Edge(id = "1000001", coordinates = { @Loc(value = WAY1_NODE1),
+                            @Loc(value = WAY1_WAY2_INTERSECTION) }, tags = "highway=service"),
+                    @Edge(id = "2000001", coordinates = { @Loc(value = WAY1_WAY2_INTERSECTION),
+                            @Loc(value = WAY2_NODE2) }, tags = "highway=primary") })
+    private Atlas truePositiveSuddenHighwayTypeChangeCheck;
+
+    @TestAtlas(nodes = { @Node(coordinates = @Loc(value = WAY1_NODE1)),
+            @Node(coordinates = @Loc(value = WAY1_WAY2_INTERSECTION)),
+            @Node(coordinates = @Loc(value = WAY2_NODE2)) }, edges = {
+                    @Edge(id = "3000001", coordinates = { @Loc(value = WAY1_NODE1),
+                            @Loc(value = WAY1_WAY2_INTERSECTION) }, tags = "highway=tertiary"),
+                    @Edge(id = "4000001", coordinates = { @Loc(value = WAY1_WAY2_INTERSECTION),
+                            @Loc(value = WAY2_NODE2) }, tags = "highway=secondary") })
+    private Atlas falsePositiveSuddenHighwayTypeChangeCheck;
+
+    @TestAtlas(nodes = { @Node(coordinates = @Loc(value = WAY1_NODE1)),
+            @Node(coordinates = @Loc(value = WAY1_WAY2_INTERSECTION)),
+            @Node(coordinates = @Loc(value = WAY2_NODE2)) }, edges = {
+                    @Edge(id = "5000001", coordinates = { @Loc(value = WAY1_NODE1),
+                            @Loc(value = WAY1_WAY2_INTERSECTION) }, tags = "highway=motorway"),
+                    @Edge(id = "6000001", coordinates = { @Loc(value = WAY1_WAY2_INTERSECTION),
+                            @Loc(value = WAY2_NODE2) }, tags = "highway=tertiary") })
+    private Atlas truePositiveCase1;
+
+    @TestAtlas(nodes = { @Node(coordinates = @Loc(value = WAY1_NODE1)),
+            @Node(coordinates = @Loc(value = WAY1_WAY2_INTERSECTION)),
+            @Node(coordinates = @Loc(value = WAY2_NODE2)) }, edges = {
+                    @Edge(id = "7000001", coordinates = { @Loc(value = WAY1_NODE1),
+                            @Loc(value = WAY1_WAY2_INTERSECTION) }, tags = "highway=primary_link"),
+                    @Edge(id = "8000001", coordinates = { @Loc(value = WAY1_WAY2_INTERSECTION),
+                            @Loc(value = WAY2_NODE2) }, tags = "highway=residential") })
+    private Atlas truePositiveCase2;
+
+    @TestAtlas(nodes = { @Node(coordinates = @Loc(value = WAY1_NODE1)),
+            @Node(coordinates = @Loc(value = WAY1_WAY2_INTERSECTION)),
+            @Node(coordinates = @Loc(value = WAY2_NODE2)) }, edges = {
+                    @Edge(id = "9000001", coordinates = { @Loc(value = WAY1_NODE1),
+                            @Loc(value = WAY1_WAY2_INTERSECTION) }, tags = "highway=tertiary"),
+                    @Edge(id = "10000001", coordinates = { @Loc(value = WAY1_WAY2_INTERSECTION),
+                            @Loc(value = WAY2_NODE2) }, tags = "highway=service") })
+    private Atlas truePositiveCase3;
+
+    public Atlas falsePositiveSuddenHighwayTypeChangeCheck()
+    {
+        return this.falsePositiveSuddenHighwayTypeChangeCheck;
+    }
+
+    public Atlas truePositiveCase1()
+    {
+        return this.truePositiveCase1;
+    }
+
+    public Atlas truePositiveCase2()
+    {
+        return this.truePositiveCase2;
+    }
+
+    public Atlas truePositiveCase3()
+    {
+        return this.truePositiveCase3;
+    }
+
+    public Atlas truePositiveSuddenHighwayTypeChangeCheck()
+    {
+        return this.truePositiveSuddenHighwayTypeChangeCheck;
+    }
+}
