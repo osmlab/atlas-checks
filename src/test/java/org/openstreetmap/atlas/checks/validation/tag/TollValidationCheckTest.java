e9d14f87365f3fe0230775b549d1de36427c6435
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2020-12-02 11:06:40 -0800

    tollValidationCheck (#432)
    * new atlas check for tollescape
    
    * tollEscapeCheck first iteration
    
    * 3 case toll validation check
    
    * 3 case check, finishing unit tests.
    
    * spotless apply
    
    * removing dockerfile to exclude it
    
    * updated instructions, status: in false positive analysis phase.
    
    * integration test fixes
    
    * integration test fixes
    
    * spotless apply and successful test.
    
    * removing prints
    
    * updating equals --> equalsIgnoreCase
    
    * code smell cleanup
    
    * code smells continued
    
    * code smells continued
    
    * code smells continued
    
    * code smells continued
    
    * code smells continued
    
    * code smells continued
    
    * code smells continued
    
    * fixing config
    
    * addressed comments regarding tollValidationCheck and added serislVersionUID for suddenHighwayTypeChangeCheck
    
    * applied all rcommendations successfully.
    
    * updated naming convention for angle related variables.
    
    * had to update config variable and test config value to reflect updated variable names.
    
    * new config value for limiting recursion while searching for nearby toll features and added more documentation for the config values..
    
    * Fixed naming of angle difference function and addressed comments
    
    * spotless apply
    
    * Added auto fix suggestions for case 1 and 2

---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/tag/TollValidationCheckTest.java
@@ -0,0 +1,57 @@
+package org.openstreetmap.atlas.checks.validation.tag;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.atlas.checks.configuration.ConfigurationResolver;
+import org.openstreetmap.atlas.checks.flag.CheckFlag;
+import org.openstreetmap.atlas.checks.validation.verifier.ConsumerBasedExpectedCheckVerifier;
+
+/**
+ * Tests for {@link TollValidationCheck}
+ * 
+ * @author v-garei
+ */
+public class TollValidationCheckTest
+{
+
+    @Rule
+    public TollValidationCheckTestRule setup = new TollValidationCheckTestRule();
+
+    @Rule
+    public ConsumerBasedExpectedCheckVerifier verifier = new ConsumerBasedExpectedCheckVerifier();
+
+    private final TollValidationCheck check = new TollValidationCheck(
+            ConfigurationResolver.inlineConfiguration("{\"TollValidationCheck\": " + "{"
+                    + "\"minimumHighwayType\": \"tertiary\","
+                    + "\"maxAngleDiffForContiguousWays\": 40.0," + "\"minInAndOutEdges\": 1.0,"
+                    + "\"maxIterationForNearbySearch\": 15.0}}"));
+
+    private static void verifyFixSuggestions(final CheckFlag flag, final int count)
+    {
+        Assert.assertEquals(count, flag.getFixSuggestions().size());
+    }
+
+    @Test
+    public void escapableWayNeedsTollTagRemoved()
+    {
+        this.verifier.actual(this.setup.escapableWayNeedsTollTagRemoved(), this.check);
+        this.verifier.verifyExpectedSize(2);
+    }
+
+    @Test
+    public void inconsistentTollTags()
+    {
+        this.verifier.actual(this.setup.inconsistentTollTags(), this.check);
+        this.verifier.verifyExpectedSize(1);
+        this.verifier.verify(flag -> verifyFixSuggestions(flag, 1));
+    }
+
+    @Test
+    public void intersectingTollFeatureWithoutTag()
+    {
+        this.verifier.actual(this.setup.intersectingTollFeatureWithoutTag(), this.check);
+        this.verifier.verifyExpectedSize(1);
+        this.verifier.verify(flag -> verifyFixSuggestions(flag, 1));
+    }
+}
