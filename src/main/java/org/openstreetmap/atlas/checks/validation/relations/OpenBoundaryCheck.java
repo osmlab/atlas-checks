5f6ba8f8901a267fe087d7fd4ca30e8f8c8f6fcf
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2020-12-07 21:17:24 -0800

    Passed unit tests.
    pased unit tests.

--- src/main/java/org/openstreetmap/atlas/checks/validation/relations/OpenBoundaryCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/relations/OpenBoundaryCheck.java
@@ -1,9 +1,15 @@
 package org.openstreetmap.atlas.checks.validation.relations;
 
-import org.apache.zookeeper.Op;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.openstreetmap.atlas.checks.base.BaseCheck;
 import org.openstreetmap.atlas.checks.flag.CheckFlag;
-import org.openstreetmap.atlas.exception.CoreException;
 import org.openstreetmap.atlas.geography.Location;
 import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
 import org.openstreetmap.atlas.geography.atlas.items.Relation;
@@ -15,17 +21,8 @@ import org.openstreetmap.atlas.tags.SyntheticRelationMemberAdded;
 import org.openstreetmap.atlas.tags.annotations.validation.Validators;
 import org.openstreetmap.atlas.utilities.configuration.Configuration;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 /**
- * OpenBoundaryCheck
- * This check flags boundaries that should be closed polygons but are not.
+ * OpenBoundaryCheck This check flags boundaries that should be closed polygons but are not.
  *
  * @author v-garei
  */
@@ -33,7 +30,8 @@ public class OpenBoundaryCheck extends BaseCheck<Long>
 {
     private static final long serialVersionUID = 6655863145391887741L;
     private static final String OPEN_BOUNDARY_INSTRUCTIONS = "The Multipolygon relation {0,number,#} with members : {1} is not closed at some locations : {2}";
-    private static final List<String> FALLBACK_INSTRUCTIONS = Collections.singletonList(OPEN_BOUNDARY_INSTRUCTIONS);
+    private static final List<String> FALLBACK_INSTRUCTIONS = Collections
+            .singletonList(OPEN_BOUNDARY_INSTRUCTIONS);
     private static final RelationOrAreaToMultiPolygonConverter RELATION_OR_AREA_TO_MULTI_POLYGON_CONVERTER = new RelationOrAreaToMultiPolygonConverter();
 
     /**
@@ -55,12 +53,11 @@ public class OpenBoundaryCheck extends BaseCheck<Long>
     @Override
     public boolean validCheckForObject(final AtlasObject object)
     {
-        Map<String, String> osmKeys = object.getOsmTags();
-        return object instanceof Relation
-                && !isFlagged(object.getOsmIdentifier())
+        final Map<String, String> osmKeys = object.getOsmTags();
+        return object instanceof Relation && !isFlagged(object.getOsmIdentifier())
                 && Validators.isOfType(object, RelationTypeTag.class, RelationTypeTag.BOUNDARY)
                 && !SyntheticRelationMemberAdded.hasAddedRelationMember(object)
-                && hasAdminLevelTag(osmKeys);
+                && this.hasAdminLevelTag(osmKeys);
     }
 
     /**
@@ -74,26 +71,27 @@ public class OpenBoundaryCheck extends BaseCheck<Long>
     protected Optional<CheckFlag> flag(final AtlasObject object)
     {
         markAsFlagged(object.getOsmIdentifier());
-        Relation relation = ((Relation) object);
+        final Relation relation = (Relation) object;
 
-        if (hasAllMembersLoaded(relation))
+        if (this.hasAllMembersLoaded(relation))
         {
-            try {
+            try
+            {
                 RELATION_OR_AREA_TO_MULTI_POLYGON_CONVERTER.convert(relation);
                 return Optional.empty();
             }
             catch (final MultiplePolyLineToPolygonsConverter.OpenPolygonException exception)
             {
                 final List<Location> openLocations = exception.getOpenLocations();
-                final Set<String> latllonSet = new HashSet<>();
+                final Set<String> latlonSet = new HashSet<>();
 
-                for (Location openLocation : openLocations)
+                for (final Location openLocation : openLocations)
                 {
-                    //create function to get lat lon - pretty it up
-                    String lat = openLocation.getLatitude().toString();
-                    String lon = openLocation.getLongitude().toString();
-                    String latlon = lat + "," + lon;
-                    latllonSet.add(latlon);
+                    // create function to get lat lon - pretty it up
+                    final String lat = openLocation.getLatitude().toString();
+                    final String lon = openLocation.getLongitude().toString();
+                    final String latlon = lat + "," + lon;
+                    latlonSet.add(latlon);
 
                 }
 
@@ -103,9 +101,8 @@ public class OpenBoundaryCheck extends BaseCheck<Long>
 
                 if (!openLocations.isEmpty())
                 {
-                    return Optional.of(this.createFlag(object,
-                            this.getLocalizedInstruction(0,
-                                    relation.getOsmIdentifier(), memberIds, latllonSet)));
+                    return Optional.of(this.createFlag(object, this.getLocalizedInstruction(0,
+                            relation.getOsmIdentifier(), memberIds, latlonSet)));
 
                 }
             }
@@ -126,7 +123,9 @@ public class OpenBoundaryCheck extends BaseCheck<Long>
 
     /**
      * Check if the tags contain admin_level OSM tag
-     * @param tags osm tags
+     * 
+     * @param tags
+     *            osm tags
      * @return boolean if tags contain admin_level tag
      */
     private boolean hasAdminLevelTag(final Map<String, String> tags)
@@ -135,12 +134,14 @@ public class OpenBoundaryCheck extends BaseCheck<Long>
     }
 
     /**
-     * Checks to make sure relation is fully loaded by checking if the
-     * all known members count is the same as the amount of members the relation in question has
-     * @param relation relational boundary in question
+     * Checks to make sure relation is fully loaded by checking if the all known members count is
+     * the same as the amount of members the relation in question has
+     * 
+     * @param relation
+     *            relational boundary in question
      * @return true if relation is fully loaded from shards.
      */
-    private boolean hasAllMembersLoaded(Relation relation)
+    private boolean hasAllMembersLoaded(final Relation relation)
     {
         return relation.members().size() == relation.allKnownOsmMembers().size();
     }
---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/relations/OpenBoundaryCheckTestRule.java
@@ -0,0 +1,88 @@
+package org.openstreetmap.atlas.checks.validation.relations;
+
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Edge;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Node;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Relation;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Relation.Member;
+
+/**
+ * Tests for {@link OpenBoundaryCheck}
+ *
+ * @author v-garei
+ */
+public class OpenBoundaryCheckTestRule extends CoreTestRule
+{
+
+    private static final String POLYGON_NODE_1 = "47.6367419, -122.2342792";
+    private static final String POLYGON_NODE_2 = "47.6370622, -122.2360741";
+    private static final String POLYGON_NODE_3 = "47.6372561, -122.2370973";
+
+    private static final String POLYGON_2_NODE_1 = "47.6364969, -122.2379916";
+    private static final String POLYGON_2_NODE_2 = "47.6365008, -122.2371735";
+    private static final String POLYGON_2_NODE_3 = "47.6357896, -122.2371660";
+    private static final String POLYGON_2_NODE_4 = "47.6357857, -122.2379841";
+
+    private static final String RELATION_ID_OPEN_MULTIPOLYGON = "123";
+    private static final String RELATION_ID_OPEN_MULTIPOLYGON2 = "124";
+
+    @TestAtlas(
+            // nodes
+            nodes = { @Node(coordinates = @Loc(value = POLYGON_2_NODE_1)),
+                    @Node(coordinates = @Loc(value = POLYGON_2_NODE_2)),
+                    @Node(coordinates = @Loc(value = POLYGON_2_NODE_3)),
+                    @Node(coordinates = @Loc(value = POLYGON_2_NODE_4)) },
+            // lines
+            edges = {
+                    @Edge(id = "2000001", coordinates = { @TestAtlas.Loc(value = POLYGON_2_NODE_1),
+                            @Loc(value = POLYGON_2_NODE_2) }, tags = { "natural=water" }),
+                    @Edge(id = "2000002", coordinates = { @TestAtlas.Loc(value = POLYGON_2_NODE_2),
+                            @Loc(value = POLYGON_2_NODE_3) }, tags = { "natural=water" }),
+                    @Edge(id = "2000003", coordinates = { @TestAtlas.Loc(value = POLYGON_2_NODE_3),
+                            @Loc(value = POLYGON_2_NODE_4) }, tags = { "natural=water" }),
+                    @Edge(id = "2000004", coordinates = { @TestAtlas.Loc(value = POLYGON_2_NODE_4),
+                            @Loc(value = POLYGON_2_NODE_1) }, tags = { "natural=water" }) },
+            // relations
+            relations = { @Relation(id = RELATION_ID_OPEN_MULTIPOLYGON2, members = {
+                    @Member(id = "2000001", type = "edge", role = "outer"),
+                    @Member(id = "2000002", type = "edge", role = "outer"),
+                    @Member(id = "2000003", type = "edge", role = "outer"),
+                    @Member(id = "2000004", type = "edge", role = "outer") }, tags = {
+                            "type=boundary", "admin_level=3" }) })
+
+    private Atlas polygonClosedFalsePositive;
+
+    @TestAtlas(
+            // nodes
+            nodes = { @Node(coordinates = @Loc(value = POLYGON_NODE_1)),
+                    @Node(coordinates = @Loc(value = POLYGON_NODE_2)),
+                    @Node(coordinates = @Loc(value = POLYGON_NODE_3)) },
+            // lines
+            edges = {
+                    @Edge(id = "1000001", coordinates = { @TestAtlas.Loc(value = POLYGON_NODE_1),
+                            @Loc(value = POLYGON_NODE_2) }),
+                    @Edge(id = "1000002", coordinates = { @TestAtlas.Loc(value = POLYGON_NODE_2),
+                            @Loc(value = POLYGON_NODE_3) }), },
+            // relations
+            relations = { @Relation(id = RELATION_ID_OPEN_MULTIPOLYGON, members = {
+                    @Member(id = "1000001", type = "edge", role = "outer"),
+                    @Member(id = "1000002", type = "edge", role = "outer"), }, tags = {
+                            "type=boundary", "admin_level=3" }) })
+
+    private Atlas polygonNotClosedTruePositive;
+
+
+    public Atlas polygonClosedFalsePositive()
+    {
+        return this.polygonClosedFalsePositive;
+    }
+
+    public Atlas polygonNotClosedTruePositive()
+    {
+        return this.polygonNotClosedTruePositive;
+    }
+
+}
