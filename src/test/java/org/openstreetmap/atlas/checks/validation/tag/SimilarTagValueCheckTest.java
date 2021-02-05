2454520d7c6284515beea43aadbb5dce058d6c48
brianjor <22751140+brianjor@users.noreply.github.com>
2021-02-05 10:06:54 -0800

    Tests

---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/tag/SimilarTagValueCheckTest.java
@@ -0,0 +1,47 @@
+package org.openstreetmap.atlas.checks.validation.tag;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.atlas.checks.configuration.ConfigurationResolver;
+import org.openstreetmap.atlas.checks.validation.verifier.ConsumerBasedExpectedCheckVerifier;
+
+/**
+ * Tests for {@link SimilarTagValueCheck}
+ *
+ * @author v-brjor
+ */
+public class SimilarTagValueCheckTest
+{
+    @Rule
+    public SimilarTagValueCheckTestRule setup = new SimilarTagValueCheckTestRule();
+
+    @Rule
+    public ConsumerBasedExpectedCheckVerifier verifier = new ConsumerBasedExpectedCheckVerifier();
+
+    private final SimilarTagValueCheck check = new SimilarTagValueCheck(ConfigurationResolver
+            .inlineConfiguration(
+                "{" +
+                    "\"SimilarTagValueCheck\": {" +
+                        "\"similarity.threshold\": {" +
+                            "\"min\": " + 0.0 + "," +
+                            "\"max\": " + 1.0 +
+                        "}," +
+                        "\"value.length.min\": " + 4.0 +
+                    "}" +
+                "}"));
+
+    @Test
+    public void testHasDuplicateTagValue()
+    {
+        this.verifier.actual(this.setup.getHasDuplicateTagTest(), this.check);
+        this.verifier.verify(flag -> Assert.assertEquals("1. The tag \"hasDupe\" contains duplicate values: [(dupe,dupe,0)]", flag.getInstructions()));
+    }
+
+    @Test
+    public void testHasSimilarTagValue()
+    {
+        this.verifier.actual(this.setup.getHasSimilarTagTest(), this.check);
+        this.verifier.verify(flag -> Assert.assertEquals("1. The tag \"hasSimilar\" contains similar values: [(similar,similer,1)]", flag.getInstructions()));
+    }
+}
---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/tag/SimilarTagValueCheckTestRule.java
@@ -0,0 +1,40 @@
+package org.openstreetmap.atlas.checks.validation.tag;
+
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Node;
+
+/**
+ * Tests for {@link SimilarTagValueCheck}
+ *
+ * @author v-brjor
+ */
+public class SimilarTagValueCheckTestRule extends CoreTestRule
+{
+    private static final String TEST_1 = "0, 0";
+
+    @TestAtlas(
+            nodes = {
+                    @Node(coordinates = @Loc(value = TEST_1), tags = { "hasDupe=dupe;dupe"})
+            }
+    )
+    private Atlas hasDuplicateTagTest;
+
+    @TestAtlas(
+            nodes = {
+                    @Node(coordinates = @Loc(value = TEST_1), tags = { "hasSimilar=similar;similer"})
+            }
+    )
+    private Atlas hasSimilarTagTest;
+
+    public Atlas getHasDuplicateTagTest()
+    {
+        return this.hasDuplicateTagTest;
+    }
+
+    public Atlas getHasSimilarTagTest() {
+        return this.hasSimilarTagTest;
+    }
+}
