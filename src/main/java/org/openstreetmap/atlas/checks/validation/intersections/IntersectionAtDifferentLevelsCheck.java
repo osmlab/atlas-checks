8bd9e453c444a485b67f61984ed9573edaf9e455
Vladimir Lemberg <vlemberg@fb.com>
2021-02-18 14:16:03 -0800

    style.

--- src/main/java/org/openstreetmap/atlas/checks/validation/intersections/IntersectionAtDifferentLevelsCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/intersections/IntersectionAtDifferentLevelsCheck.java
@@ -32,8 +32,9 @@ public class IntersectionAtDifferentLevelsCheck extends BaseCheck<Long>
     private static final String INSTRUCTION_FORMAT = "The Node id {0,number,#} connects two Ways at different levels.";
     private static final List<String> FALLBACK_INSTRUCTIONS = Collections
             .singletonList(INSTRUCTION_FORMAT);
-    /* default filter is empty, however to minimize the scope for great separation cases only, use below filter.
-       example: "bridge->yes|tunnel->yes|embankment->yes|cutting->yes|ford->yes"
+    /*
+     * default filter is empty, however to minimize the scope for great separation cases only, use
+     * below filter. example: "bridge->yes|tunnel->yes|embankment->yes|cutting->yes|ford->yes"
      */
     private static final String GREAT_SEPARATION_FILTER_DEFAULT = "";
     private final TaggableFilter greatSeparationFilter;
