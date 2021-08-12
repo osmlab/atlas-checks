824b8ff7659508bba4f374a56bb107b66a4bfca6
Nelli Aydinyan <55819977+nelkapelmenka@users.noreply.github.com>
2021-08-11 17:22:25 -0700

    [new] StreetNameCheck (#581)
    * streenamecheck logic
    
    * streetnamecheck config
    
    * streetnamecheck push2
    
    * indexoutofboundsexception
    
    * indexoutofbounds error for line 120
    
    * fir flagging
    
    * nomenklature
    
    * renamed vars
    
    * DEU tagging of associated street
    
    * fixing deprecated
    
    * raw highwayaccesscheck
    
    * highwayaccesscheck code
    
    * streetnamecheck tests
    
    * updated config
    
    * added StreetNameCheck
    
    * deleted highwayaccesscheck
    
    * docker adjustments
    
    * build successful
    
    * suppressions
    
    * redo builds
    
    * substituted unicode and deleted suppresion
    
    * deleted fixme comment
    
    * updated pr
    
    * delete deprecated tag description
    
    * added a unit test with inlineconfig
    
    Co-authored-by: Nelli Aydinyan (INSIGHT GLOBAL INC) <v-naydinyan@microsoft.com>

---
+++ src/test/java/org/openstreetmap/atlas/checks/validation/tag/StreetNameCheckTest.java
@@ -0,0 +1,205 @@
+package org.openstreetmap.atlas.checks.validation.tag;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.atlas.checks.configuration.ConfigurationResolver;
+import org.openstreetmap.atlas.checks.validation.verifier.ConsumerBasedExpectedCheckVerifier;
+
+/**
+ * Tests for {@link StreetNameCheck}
+ *
+ * @author v-naydinyan
+ */
+public class StreetNameCheckTest
+{
+    @Rule
+    public StreetNameCheckTestRule setup = new StreetNameCheckTestRule();
+
+    @Rule
+    public ConsumerBasedExpectedCheckVerifier verifier = new ConsumerBasedExpectedCheckVerifier();
+
+    private final StreetNameCheck check = new StreetNameCheck(
+            ConfigurationResolver.emptyConfiguration());
+
+    @Test
+    public void falsePositiveAutNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveAutNodeInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveAutRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveAutRelationInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveAutWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveAutWayInvalidTag(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveCheNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveCheNodeInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveCheRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveCheRelationInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveCheWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveCheWayInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveDeuNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveDeuNodeInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveDeuRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveDeuRelationInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveDeuWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveDeuWayInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveLieNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveLieNodeInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveLieRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveLieRelationInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void falsePositiveLieWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.falsePositiveLieWayInvalidValue(), this.check);
+        this.verifier.verifyEmpty();
+    }
+
+    @Test
+    public void truePositiveAutNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveAutNodeInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveAutRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveAutRelationInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveAutWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveAutWayInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveCheNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveCheNodeInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveCheRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveCheRelationInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveCheWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveCheWayInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveDeuNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveDeuNodeInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveDeuRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveDeuRelationInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveDeuWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveDeuWayInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveLieNodeInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveLieNodeInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveLieRelationInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveLieRelationInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    @Test
+    public void truePositiveLieWayInvalidValue()
+    {
+        this.verifier.actual(this.setup.truePositiveLieWayInvalidValue(), this.check);
+        this.verifier.verifyExpectedSize(1);
+    }
+
+    /**
+     * This test removes the notContainsValue for a German configuration. The item that was
+     * falsePositive is now truePositive.
+     */
+    @Test
+    public void truePositiveTestWithInlineConfig()
+    {
+        final StreetNameCheck thisTest = new StreetNameCheck(
+                ConfigurationResolver.inlineConfiguration(
+                        "{\"StreetNameCheck\":{\"check\":{\"countries\":[\"DEU\"], \"containsValues\":[[\"strasse\"]], \"notContainsValues\":[[]]}}}"));
+        this.verifier.actual(this.setup.falsePositiveDeuNodeInvalidValue(), thisTest);
+        this.verifier.verifyExpectedSize(1);
+    }
+}
