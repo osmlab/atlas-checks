c10c20990c0dade96d01c85ef2bd83348e34f497
mselaineshum <84435178+mselaineshum@users.noreply.github.com>
2021-10-29 15:58:03 -0700

    Roundabout Highway Tag Check

--- src/test/java/org/openstreetmap/atlas/checks/validation/linear/edges/RoundaboutHighwayTagCheckTest.java
+++ src/test/java/org/openstreetmap/atlas/checks/validation/linear/edges/RoundaboutHighwayTagCheckTest.java
@@ -20,6 +20,38 @@ public class RoundaboutHighwayTagCheckTest
     public ConsumerBasedExpectedCheckVerifier verifier = new ConsumerBasedExpectedCheckVerifier();
 
     @Test
+    public void roundaboutWithHighwayTagOneTest()
+    {
+        this.verifier.actual(this.setup.roundaboutWithHighwayTagOneAtlas(),
+                new RoundaboutHighwayTagCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(0, flags.size()));
+    }
+
+    @Test
+    public void roundaboutWithHighwayTagFiveTest()
+    {
+        this.verifier.actual(this.setup.roundaboutWithHighwayTagFiveAtlas(),
+                new RoundaboutHighwayTagCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(1, flags.size()));
+    }    
+
+    @Test
+    public void roundaboutWithHighwayTagThreeTest()
+    {
+        this.verifier.actual(this.setup.roundaboutWithHighwayTagThreeAtlas(),
+                new RoundaboutHighwayTagCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(0, flags.size()));
+    }    
+
+    @Test
+    public void roundaboutWithHighwayTagTwoTest()
+    {
+        this.verifier.actual(this.setup.roundaboutWithHighwayTagTwoAtlas(),
+                new RoundaboutHighwayTagCheck(ConfigurationResolver.emptyConfiguration()));
+        this.verifier.globallyVerify(flags -> Assert.assertEquals(1, flags.size()));
+    }
+
+    @Test
     public void roundaboutWithHighwayTagZeroTest()
     {
         this.verifier.actual(this.setup.roundaboutWithHighwayTagZeroAtlas(),
